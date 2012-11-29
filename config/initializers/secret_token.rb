b39e5e550b11283e1c6bc0569d9852b6da92a851
profmaad <prof.maad@lambda-bb.de>
2012-11-29 17:00:54 +0100

    Add basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudyServer::Application.config.secret_token = '3ecf7ee86646efba1df994e1675cc61208f9702924ae89c7eb92f3716b5922f1f0df6e6438f0ca03d18e0fc0e5d383ee52542f09b0c99d8f53693fd61e8d2c18'
