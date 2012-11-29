0f3d0942a656cf5e75705e93f1348272b5ebf748
profmaad <prof.maad@lambda-bb.de>
2012-11-29 17:25:27 +0100

    Add session controller skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 StudyServer::Application.routes.draw do
+  get "session/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ StudyServer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'session#index'
 
   # See how all your routes lay out with "rake routes"
 
