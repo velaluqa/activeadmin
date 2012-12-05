6f4f7f29b0a6d29627783f95a0c4bbdc59d238f5
profmaad <prof.maad@lambda-bb.de>
2012-12-05 17:41:49 +0100

    Implement form including

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -11,11 +11,17 @@ class FormsController < ApplicationController
   def show
     @form_config = parse_form_config(params[:id])
     return if @form_config.nil?
-    @form_config_components = form_config_components(params[:id])
-    return if @form_config_components.nil?
+    @form_components = form_components(params[:id])
+    return if @form_components.nil?
+
+    @form_config, @form_components = process_imports(@form_config, @form_components, [@form.id])
   end
 
 protected
+
+  def form_by_name(name)
+    return Form.where('name = ?', name).first
+  end
 
   def find_record
     begin
@@ -30,37 +36,65 @@ protected
     begin
       config = YAML.load_file(Rails.application.config.form_configs_directory + "/#{id}.yml")
     rescue Errno::ENOENT
-      flash[:error] = 'Form configuration is missing'
+      flash[:error] = 'Form configuration for form '#{id}' is missing'
       redirect_to :action => 'index'
     rescue
-      flash[:error] = 'Form configuration is invalid'
+      flash[:error] = 'Form configuration for form '#{id}' is invalid'
       redirect_to :action => 'index'
     end
 
     return config
   end
 
-  def read_form_config(id)
-    begin
-      data = File.open() {|f| f.read}
-    rescue
-      flash[:error] = 'Form configuration is missing'
-      redirect_to :action => 'index'
-    end
-
-    return data
-  end
-
-  def form_config_components(id)
-    components = {}
+  def form_components(id)
+    components = {:validators => [], :stylesheets => []}
 
     if(File.exists?(Rails.application.config.form_configs_directory + "/#{id}.js"))
-      components[:validators] = File.open(Rails.application.config.form_configs_directory + "/#{id}.js", 'o') {|f| f.read}
+      components[:validators] << File.open(Rails.application.config.form_configs_directory + "/#{id}.js", 'o') {|f| f.read}
     end
     if(File.exists?(Rails.application.config.form_configs_directory + "/#{id}.css"))
-      components[:stylesheet] = File.open(Rails.application.config.form_configs_directory + "/#{id}.css", 'o') {|f| f.read}
+      components[:stylesheet] << File.open(Rails.application.config.form_configs_directory + "/#{id}.css", 'o') {|f| f.read}
     end
 
     return components
+  end
+
+  def process_imports(config, components, already_included)
+    full_config = []
+    full_components = Hash.new(components)
+
+    config.each do |field|
+      if field['include'].nil?
+        full_config << field
+      else
+        included_form = form_by_name(field['include'])
+        if included_form.nil?
+          flash[:error] = "The form tried to include form '#{field['include']}', which doesn't exist"
+          redirect_to :action => 'index'
+        end
+        if already_included.include?(included_form.id)
+          flash[:error] = "The form has a circular include of form '#{included_form.name}'"
+          redirect_to :action => 'index'
+        end
+
+        already_included << included_form.id
+
+        included_config = parse_form_config(included_form.id)
+        included_components = form_components(included_form.id)
+        
+        included_config, included_components = process_imports(included_config, included_components, already_included)
+
+        full_config += included_config
+        full_components.each do |key, value|
+          full_components[k] = v + included_components[k]
+        end
+      end
+    end
+
+    full_components.each do |key, value|
+      full_components[k] = value.join("\n")
+    end    
+
+    return [full_config, full_components]
   end
 end
