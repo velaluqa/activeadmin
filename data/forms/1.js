41fc833cb7ac1a1580983b614990295c5feab5fb
profmaad <prof.maad@lambda-bb.de>
2012-12-07 14:25:40 +0100

    More testing JS

--- data/forms/1.js
+++ data/forms/1.js
@@ -1,12 +1,33 @@
+$(document).ready(function()
+{
+	if(typeof PharmTraceAPI != 'undefined')
+	{
+		console.log("PharmTrace API availale");
+	}
+	else
+	{
+		console.log("ERROR: PharmTrace API not available, form will not be fully functional!");
+	}
+});
+
 $(function () {
 	   $("input,textarea,select").jqBootstrapValidation(
 	   {
 		submitError: function($form, event, errors)
 		{
-			alert(JSON.stringify(PharmTraceAPI, null, 4));
-
 			PharmTraceAPI.updateROIs();
-			alert(JSON.stringify(PharmTraceAPI.roiList, null, 4));
+
+			rois = PharmTraceAPI.rois;
+			alert(rois.constructor.name);
+			alert(rois.length);
+
+			for(var i = 0; i < rois.length; i++)
+			{
+				for(var key in rois[i])
+				{
+					alert(key+ ": "+rois[i][key]);
+				}
+			}
 		}
 	}
 	);
