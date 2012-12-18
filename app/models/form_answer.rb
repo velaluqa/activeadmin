1d41aa62630eb1cbff7d6a5a4dc92bb1c3ee9b7e
profmaad <prof.maad@lambda-bb.de>
2012-12-18 16:51:18 +0100

    Store signature as string (base64), remove form timestamp (obsolete, since form_id is now 1:1 related to a form version)

--- app/models/form_answer.rb
+++ app/models/form_answer.rb
@@ -4,8 +4,7 @@ class FormAnswer
   key :reader, Integer
   key :read, Integer
   key :form_id, Integer
-  key :form_timestamp, Integer
   key :submitted_at, Time
   key :answers, Hash
-  key :signature, Binary
+  key :signature, String
 end
