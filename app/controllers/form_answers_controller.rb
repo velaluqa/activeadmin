b701b3908cbb1ff87651747d13d4dd1d4dd7bb05
profmaad <prof.maad@lambda-bb.de>
2012-12-18 16:52:32 +0100

    Store signature and form_id into proper fields

--- app/controllers/form_answers_controller.rb
+++ app/controllers/form_answers_controller.rb
@@ -3,11 +3,11 @@ class FormAnswersController < ApplicationController
 
   def create
     answer = FormAnswer.new
+    answer.signature = params[:form_answer].delete('signature')
+    answer.form_id = params[:form_answer].delete('form_id')
     answer.answers = params[:form_answer]
-    answer.reader = 23
+    answer.reader = current_user.id
     answer.read = 42
-    answer.form_id = 1
-    answer.form_timestamp = Time.now
     answer.submitted_at = Time.now
 
     answer.save
