b9a7760d7e1ed684ff40e3498409279ff9d1c402
profmaad <prof.maad@lambda-bb.de>
2012-12-21 17:57:46 +0100

    Basic resource models for ActiveAdmin

---
+++ app/admin/form_answers.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register FormAnswer do
+  
+end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :name do |user|
+      link_to user.name, admin_user_path(user)
+    end
+    column :email
+    default_actions
+  end
 end
