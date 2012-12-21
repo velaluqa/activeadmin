544ebc7ad5878fc17aa508c67546016489c451ce
profmaad <prof.maad@lambda-bb.de>
2012-12-21 16:14:09 +0100

    Implement basic permission model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,29 +4,32 @@ class Ability
   def initialize(user)
     return if user.nil? # guest users have no access whatsoever
 
-    
+    is_app_admin? = !(user.roles.first(:conditions => { :object_type => nil, :object_id => nil, :role => Role::role_sym_to_int(:manage) }).nil?)
 
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    # App Admin
+    if is_app_admin?
+      can :manage, User
+      can :manage, Role
+    end
+
+    # Study Admin
+    can :manage, Study do |study|
+      !(study.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:manage)}).nil?)
+    end
+
+    # Session Admin
+    can :manage, Session do |session|
+      !(session.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:manage)}).nil?)
+    end
+
+    # Validator
+    can :validate, Session do |session|
+      !(session.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:validate)}).nil?)
+    end
+
+    # Reader
+    can :blind_read, Session do |session|
+      !(session.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:blind_read)}).nil?)
+    end
   end
 end
