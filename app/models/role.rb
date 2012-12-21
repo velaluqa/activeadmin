90ce600eb28ec51f2c96b139a10a584f3eae97fb
profmaad <prof.maad@lambda-bb.de>
2012-12-21 16:13:46 +0100

    Rename blind read role to avoid clash with CRUD verbs

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,13 +4,17 @@ class Role < ActiveRecord::Base
   belongs_to :user
   belongs_to :object, :polymorphic => true  
 
-  ROLE_SYMS = [:manage, :validate, :read]
+  ROLE_SYMS = [:manage, :validate, :blind_read]
+
+  def self.role_sym_to_int(sym)
+    return Role::ROLE_SYMS.index(sym)
+  end
 
   def role
-    return ROLE_SYMS[read_attribute(:role)]
+    return Role::ROLE_SYMS[read_attribute(:role)]
   end
   def role=(sym)
-    index = ROLE_SYMS.index(sym)
+    index = Role::ROLE_SYMS.index(sym)
 
     if index.nil?
       throw "Unsupported role"
