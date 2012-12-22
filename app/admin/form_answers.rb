f8db8ac7c0d13920edbefc0895580e83184ae2d9
profmaad <prof.maad@lambda-bb.de>
2012-12-22 16:20:56 +0100

    Implement basic FormAnswers resource for ActiveAdmin

---
+++ app/admin/form_answers.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register FormAnswer do
+  before_filter { @skip_sidebar = true }
+
+  index do
+    selectable_column
+    column :user
+    column :session
+    column :form
+    column :patient
+    column :images
+    column 'Submission Date', :submitted_at
+    column :signature do |formanswer|
+      if(formanswer.signature_is_valid?)
+        status_tag('Valid', :ok)
+      else
+        status_tag('Invalid', :error)
+      end
+    end
+
+    default_actions
+  end  
+end
