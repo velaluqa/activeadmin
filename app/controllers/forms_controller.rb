8d8e6d556da86b0a0fed442baf4fe7ed48dc6b78
profmaad <prof.maad@lambda-bb.de>
2012-12-26 13:39:19 +0100

    Move form config parsing into model

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -13,131 +13,21 @@ class FormsController < ApplicationController
   end
 
   def show
-    @form_config = parse_form_config(@form)
-    return if @form_config.nil?
-    @form_components = form_components(@form)
-    return if @form_components.nil?
-
-    @form_config, @form_components, @repeatables = process_imports(@form_config, @form_components, [], [@form.id])
-    @form_components = stringify_form_components(@form_components)
-
+    @form_config, @form_components, @repeatables = @form.configuration
+    
     pp @form_config
+    puts "---------------------------------"
+    pp @form_components
+    puts "---------------------------------"
+    pp @repeatables
+    puts "---------------------------------"
+
+    return if (@form_config.nil? or @form_components.nil? or @repeatables.nil?)
   end
 
 protected
 
   def find_form_from_params
-    @form = find_form(params[:id], params[:version])
-  end
-  def find_form(name, version)
-    form = nil
-    begin
-      if version.nil?
-        form = Form.find_by_name(name, :order => "form_version DESC")
-      else
-        form = Form.find_by_name_and_form_version(name, version)
-      end
-      throw "Form does not exist" if form.nil?
-    rescue
-      flash[:error] = 'Form does not exist'
-      redirect_to :action => 'index'
-    end
-
-    return form
-  end
-
-  def parse_form_config(form)
-    begin
-      config = YAML.load_file(Rails.application.config.form_configs_directory + "/#{form.id}.yml")
-    rescue Errno::ENOENT
-      flash[:error] = "Form configuration for form '#{form.id}' (name: #{form.name}, version: #{form.form_version} ' is missing"
-      redirect_to :action => 'index'
-    rescue
-      flash[:error] = "Form configuration for form '#{form.id}' (name: #{form.name}, version: #{form.form_version} ' is invalid"
-      redirect_to :action => 'index'
-    end
-
-    return config
-  end
-
-  def form_components(form)
-    components = {:validators => [], :stylesheets => []}
-
-    if(File.exists?(Rails.application.config.form_configs_directory + "/#{form.id}.js"))
-      components[:validators] << File.open(Rails.application.config.form_configs_directory + "/#{form.id}.js", 'r') {|f| f.read}
-    end
-    if(File.exists?(Rails.application.config.form_configs_directory + "/#{form.id}.css"))
-      components[:stylesheet] << File.open(Rails.application.config.form_configs_directory + "/#{form.id}.css", 'r') {|f| f.read}
-    end
-
-    return components
-  end
-
-  def stringify_form_components(components)
-    components.each do |key, value|
-      components[key] = value.join("\n")
-    end
-  end
-
-  def process_imports(config, components, repeatables, already_included)
-    full_config = []
-    full_components = components
-
-    config.each do |field|
-      if field['include'].nil?
-        full_config << field
-      else
-        included_form = find_form(field['include'], field['version'])
-        if included_form.nil?
-          flash[:error] = "The form tried to include form '#{field['include']}', which doesn't exist"
-          redirect_to :action => 'index'
-        end
-        if already_included.include?(included_form.id)
-          flash[:error] = "The form has a circular include of form '#{included_form.name}'"
-          redirect_to :action => 'index'
-        end
-
-        already_included << included_form.id
-
-        included_config = parse_form_config(included_form)
-        included_components = form_components(included_form)
-        
-        included_config, included_components = process_imports(included_config, included_components, repeatables, already_included)
-
-        if(field['repeat'].nil?)
-          full_config += included_config
-        else
-          repeatable = Marshal.load(Marshal.dump(included_config))
-          repeatable.each do |included_field|
-            included_field['id'] = "#{field['repeat']['prefix']}[][#{included_field['id']}]"
-          end
-          repeatable << {'type' => 'divider'}
-
-          repeatables << {:id => field['repeat']['prefix'], :max => field['repeat']['max'], :config => repeatable}
-
-          full_config << {'type' => 'add_repeat', 'group-label' => "#{field['repeat']['label']}s", 'button-label' => "Add #{field['repeat']['label']}", 'id' => field['repeat']['prefix'], 'max' => field['repeat']['max']}
-
-          field['repeat']['min'].times do |i|
-            config_copy = Marshal.load(Marshal.dump(included_config))
-            
-            config_copy.each do |included_field|
-              included_field['id'] = "#{field['repeat']['prefix']}[#{i}][#{included_field['id']}]"
-            end
-
-            config_copy << {'type' => 'divider'}
-
-            full_config += config_copy
-          end
-
-          full_config << {'type' => 'group-end', 'id' => field['repeat']['prefix']}
-        end
-
-        full_components.each do |key, value|
-          full_components[key] = value + included_components[key]
-        end
-      end
-    end
-    
-    return [full_config, full_components, repeatables]
+    @form = Form.find_with_name_and_version(params[:id], params[:version])
   end
 end
--- app/models/form.rb
+++ app/models/form.rb
@@ -9,4 +9,131 @@ class Form < ActiveRecord::Base
 
   belongs_to :session
   has_many :form_answers
+
+  def self.find_with_name_and_version(name, version)
+    form = nil
+
+    if version.nil?
+      form = Form.where(:name => name).order("form_version DESC").first
+    else
+      form = Form.where(:name => name, :version => version).first
+    end
+    
+    return form
+  end
+
+  def configuration(already_included_forms = nil, stringify = false)
+    form_config = parse_config
+    return [nil,nil,nil] if form_config.nil?
+    form_components = components
+    return [nil,nil,nil] if form_components.nil?
+
+    already_included_forms = [] if already_included_forms.nil?
+    already_included_forms << read_attribute(:id)
+
+    form_config, form_components, repeatables = process_imports(form_config, form_components, already_included_forms)
+    form_components = stringify_form_components(form_components) if stringify
+
+    return [form_config, form_components, repeatables]
+  end
+
+  protected
+  
+  def parse_config
+    id = read_attribute(:id)
+
+    begin
+      config = YAML.load_file(Rails.application.config.form_configs_directory + "/#{id}.yml")
+    rescue Errno::ENOENT
+      flash[:error] = "Form configuration for form '#{id}' (name: #{read_attribute(:name)}, version: #{read_attribute(:form_version)} ' is missing"
+      redirect_to :action => 'index'
+    rescue
+      flash[:error] = "Form configuration for form '#{id}' (name: #{read_attribute(:name)}, version: #{read_attribute(:form_version)} ' is invalid"
+      redirect_to :action => 'index'
+    end
+    
+    return config
+  end
+  
+  def components
+    id = read_attribute(:id)
+
+    form_components = {:validators => [], :stylesheets => []}
+
+    if(File.exists?(Rails.application.config.form_configs_directory + "/#{id}.js"))
+      form_components[:validators] << File.open(Rails.application.config.form_configs_directory + "/#{id}.js", 'r') {|f| f.read}
+    end
+    if(File.exists?(Rails.application.config.form_configs_directory + "/#{id}.css"))
+      form_components[:stylesheet] << File.open(Rails.application.config.form_configs_directory + "/#{id}.css", 'r') {|f| f.read}
+    end
+
+    return form_components
+  end
+
+  def stringify_form_components(components)
+    components.each do |key, value|
+      components[key] = value.join("\n")
+    end
+  end
+
+  def process_imports(config, components, already_included)
+    full_config = []
+    full_components = components
+    repeatables = []
+
+    config.each do |field|
+      if field['include'].nil?
+        full_config << field
+      else
+        included_form = Form.find_with_name_and_version(field['include'], field['version'])
+        if included_form.nil?
+          flash[:error] = "The form tried to include form '#{field['include']}', which doesn't exist"
+          redirect_to :action => 'index'
+        end
+        if already_included.include?(included_form.id)
+          flash[:error] = "The form has a circular include of form '#{included_form.name}'"
+          redirect_to :action => 'index'
+        end
+
+#        already_included << included_form.id
+
+        included_config, included_components, included_repeatables = included_form.configuration(already_included)
+        repeatables += included_repeatables
+       
+        if(field['repeat'].nil?)
+          full_config += included_config
+        else
+          repeatable = Marshal.load(Marshal.dump(included_config))
+          repeatable.each do |included_field|
+            included_field['id'] = "#{field['repeat']['prefix']}[][#{included_field['id']}]"
+          end
+          repeatable << {'type' => 'divider'}
+
+          repeatables << {:id => field['repeat']['prefix'], :max => field['repeat']['max'], :config => repeatable}
+
+          full_config << {'type' => 'add_repeat', 'group-label' => "#{field['repeat']['label']}s", 'button-label' => "Add #{field['repeat']['label']}", 'id' => field['repeat']['prefix'], 'max' => field['repeat']['max']}
+
+          field['repeat']['min'].times do |i|
+            config_copy = Marshal.load(Marshal.dump(included_config))
+            
+            config_copy.each do |included_field|
+              included_field['id'] = "#{field['repeat']['prefix']}[#{i}][#{included_field['id']}]"
+            end
+
+            config_copy << {'type' => 'divider'}
+
+            full_config += config_copy
+          end
+
+          full_config << {'type' => 'group-end', 'id' => field['repeat']['prefix']}
+        end
+
+        full_components.each do |key, value|
+          full_components[key] = value + included_components[key]
+        end
+      end
+    end
+    
+    return [full_config, full_components, repeatables]
+  end
 end
