7e986f4445d2e6a85f34467f131d9fa51a666558
profmaad <prof.maad@lambda-bb.de>
2012-12-26 21:06:18 +0100

    Implement custom show page for form answers, including answer rendering

--- app/admin/form_answers.rb
+++ app/admin/form_answers.rb
@@ -9,8 +9,8 @@ ActiveAdmin.register FormAnswer do
     column :patient
     column :images
     column 'Submission Date', :submitted_at
-    column :signature do |formanswer|
-      if(formanswer.signature_is_valid?)
+    column :signature do |form_answer|
+      if(form_answer.signature_is_valid?)
         status_tag('Valid', :ok)
       else
         status_tag('Invalid', :error)
@@ -20,8 +20,24 @@ ActiveAdmin.register FormAnswer do
     default_actions
   end
 
-  show do
-    pp form_answer
-    pp form_answer.printable_answers
+  show do |form_answer|
+    attributes_table do
+      row :user
+      row :session
+      row :form
+      row :patient
+      row :images
+      row :submitted_at
+      row :signature do
+        if(form_answer.signature_is_valid?)
+          status_tag('Valid', :ok)
+        else
+          status_tag('Invalid', :error)
+        end
+      end
+      row :answers do
+        render "forms/results", :fields => form_answer.printable_answers, :display_type => 'review'
+      end
+    end
   end
 end
