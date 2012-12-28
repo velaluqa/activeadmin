de13360224061e26ec375dc04ea6a6ca7b074ffd
profmaad <prof.maad@lambda-bb.de>
2012-12-28 17:29:12 +0100

    Add methods to session model to retrieve config and view sequence

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Session < ActiveRecord::Base
   attr_accessible :name, :study, :user, :study_id, :user_id
 
@@ -11,4 +13,29 @@ class Session < ActiveRecord::Base
   has_many :forms
 
   scope :blind_readable_by_user, lambda { |user| where(:user_id => user.id).includes(:study) }
+
+  def configuration
+    config = YAML.load_file(Rails.application.config.session_configs_directory + "/#{id}.yml")
+    return config
+  end
+
+  def view_sequence
+    csv_options = {
+      :col_sep => ',',
+      :row_sep => :auto,
+      :quote_char => '"',
+      :headers => false,
+    }
+
+    sequence = File.open(Rails.application.config.session_configs_directory + "/#{id}.csv", 'r') do |f|
+      csv = CSV.new(f, csv_options)
+      csv.read
+    end
+
+    return sequence
+  end
+
+  private
+
+  
 end
