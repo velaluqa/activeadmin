fdd61c9a5c993cdff205a50b8438fd7245bcf45b
profmaad <prof.maad@lambda-bb.de>
2012-12-31 15:34:36 +0100

    Make all attributes of patient available (ActiveAdmin strikes again)

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ActiveRecord::Base
-  attr_accessible :images_folder, :session, :subject_id
+  attr_accessible :images_folder, :session, :subject_id, :session_id
 
   belongs_to :session
   has_many :form_answers
