270c5bbc18349f4e08252eff4bcd2156973c2fe1
profmaad <prof.maad@lambda-bb.de>
2012-12-31 15:35:41 +0100

    Custom patient ActiveAdmin config

--- app/admin/patients.rb
+++ app/admin/patients.rb
@@ -1,3 +1,28 @@
 ActiveAdmin.register Patient do
-  
+  index do
+    selectable_column
+    column :session
+    column :subject_id
+    column :images_folder
+    
+    default_actions
+  end
+
+  show do |patient|
+    attributes_table do
+      row :session
+      row :subject_id
+      row :images_folder
+    end
+  end
+
+  form do |f|
+    f.inputs 'Details' do
+      f.input :session
+      f.input :subject_id
+      f.input :images_folder
+    end
+
+    f.buttons
+  end  
 end
