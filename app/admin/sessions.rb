e7b06ac7ef4f5b9e4d224a34acd4a193850e21d9
profmaad <prof.maad@lambda-bb.de>
2013-01-02 18:40:56 +0100

    Customize Session AA resource, implement custom action to import views from CSV

--- app/admin/sessions.rb
+++ app/admin/sessions.rb
@@ -1,3 +1,43 @@
 ActiveAdmin.register Session do
+
+  index do
+    selectable_column
+
+    column :name do |session|
+      link_to session.name, admin_session_path(session)
+    end
+    column :study
+    column 'Reader', :user
+    column 'Progress' do |session|
+      "#{session.next_view_position} / #{session.view_sequence(false).size}"
+    end
+    column 'Most Recent Pause' do |session|
+      most_recent_pause = session.most_recent_pause
+      
+      if most_recent_pause.nil?
+        "None"
+      else
+        link_to most_recent_pause.end.to_s, admin_session_pause_path(most_recent_pause)
+      end      
+    end
+
+    default_actions
+  end
+
+  member_action :import_csv, :method => :post do
+    @session = Session.find(params[:id])
+
+    num_imported = View.batch_create_from_csv(params[:session][:file].tempfile, @session, @session.next_view_position)
+
+    redirect_to({:action => :show}, :notice => "Successfully imported #{num_imported} views from CSV")
+  end
+  member_action :import_csv_form, :method => :get do
+    @session = Session.find(params[:id])
+
+    render 'admin/sessions/import_csv_form'
+  end
+  action_item :only => :show do
+    link_to 'Import CSV', import_csv_form_admin_session_path(session)
+  end
   
 end
