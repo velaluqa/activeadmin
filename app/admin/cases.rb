d9fae482f997581fc32fced62e9307839d95b5e1
profmaad <prof.maad@lambda-bb.de>
2013-01-11 14:27:37 +0100

    Add form answer column to case

--- app/admin/cases.rb
+++ app/admin/cases.rb
@@ -9,6 +9,14 @@ ActiveAdmin.register Case do
     column :patient
     column :images
     column :case_type
+    column :form_answer do |c|
+      if(c.form_answer.nil?)
+        status_tag('None', :error)
+      else
+        status_tag('available', :ok, :label => link_to('Available', admin_form_answer_path(c.form_answer)).html_safe)
+        #link_to('Available', admin_form_answer_path(c.form_answer))
+      end
+    end
     
     default_actions
   end
