19f9c20f08f22d6f154a04e85bacd93600667bba
profmaad <prof.maad@lambda-bb.de>
2013-01-17 17:40:07 +0100

    Change styling of PharmTrace to pharmtrace (for activeadmin)

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "PharmTrace Study Server"
+  config.site_title = "pharmtrace Study Server"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
