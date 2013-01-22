9f379f578c298994f740ed0851b814b19440c8f3
profmaad <prof.maad@lambda-bb.de>
2013-01-22 19:30:51 +0100

    Implement ability structure to simplified prototype auth system (admin or not admin), add ability defs for full system, but don't use them (refs #211)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,44 +4,6 @@ class Ability
   def initialize(user)
     return if user.nil? # guest users have no access whatsoever
 
-    # App Admin
-    if user.is_app_admin?
-      can :manage, User
-      can :manage, Role
-    end
-
-    # Study Admin
-    can :manage, Study do |study|
-      !(study.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:manage)}).nil?)
-    end
-
-    # Session Admin
-    can :manage, Session do |session|
-      !(session.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:manage)}).nil?)
-    end
-
-    can :manage, Case do |c|
-      can? :manage, c.session
-    end
-    can :manage, CaseData do |cd|
-      can? :manage, cd.case
-    end
-    can :manage, FormAnswer do |form_answer|
-      can? :manage, form_answer.session
-    end
-    can :read, Form do |form|
-      form.session.nil?
-    end
-    can :manage, Form do |form|
-      user.is_app_admin? or (can? :manage, form.session)
-    end
-    can :manage, Patient do |patient|
-      can? :manage, patient.session
-    end
-    can :manage, PatientData do |pd|
-      can? :manage, pd.patient
-    end
-
     # Validator
     can :validate, Session do |session|
       !(session.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:validate)}).nil?)
@@ -51,5 +13,63 @@ class Ability
     can :blind_read, Session do |session|
       !(session.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:blind_read)}).nil?)
     end
+
+    # this is just for the simplified prototype auth system, either you are an admin or you're not
+    if user.is_app_admin?
+      can :manage, :all
+    end
+
+    return
+    # everything down below is for the actual, proper auth system that is not yet in place
+
+    # App Admin
+    if user.is_app_admin?
+      can :manage, :all
+      can :manage, User
+      can :manage, Role
+      can :manage, Study
+    end
+
+    # Study Admin
+    can :manage, Study do |study|
+      !(study.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:manage)}).nil?)
+    end
+
+    # Session Admin
+    can :manage, Session do |session|
+      puts "CAN MANAGE SESSION?: #{session}"
+      !(session.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:manage)}).nil?) or
+        !(session.study.roles.first(:conditions => { :user_id => user.id, :role => Role::role_sym_to_int(:manage)}).nil?)
+    end
+
+    can :manage, Case do |c|
+      puts "CAN MANAGE CASE?: #{c}"
+      can? :manage, c.session
+    end
+    can :manage, CaseData do |cd|
+      puts "CAN MANAGE CASEDATA?: #{cd}"
+      can? :manage, cd.case
+    end
+    can :manage, FormAnswer do |form_answer|
+      puts "CAN MANAGE FORMANSWER?: #{form_answer}"
+      can? :manage, form_answer.session
+    end
+    can :read, Form do |form|
+      puts "CAN READ FORM?: #{form}"
+      form.session.nil?
+    end
+    can :manage, Form do |form|
+      puts "CAN MANAGE FORM?: #{form}"
+      user.is_app_admin? or (can? :manage, form.session)
+    end
+    can :manage, Patient do |patient|
+      puts "CAN MANAGE PATIENT?: #{patient}"
+      can? :manage, patient.session
+    end
+    can :manage, PatientData do |pd|
+      puts "CAN MANAGE PATIENTDATA?: #{pd}"
+      can? :manage, pd.patient
+    end
+
   end
 end
