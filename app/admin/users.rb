4ef161ce5f1b15ac27adafff1189583b0fe64121
profmaad <prof.maad@lambda-bb.de>
2013-01-30 15:04:32 +0100

    Customize user ActiveAdmin resource, implement create form (closes #279)

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,6 +5,74 @@ ActiveAdmin.register User do
       link_to user.name, admin_user_path(user)
     end
     column :email
+    column :key_pair do |user|
+      if(user.public_key.nil? or user.private_key.nil?)
+        status_tag("Missing", :error)
+      else
+        status_tag("Present", :ok)
+      end
+    end
     default_actions
+  end
+
+  show do |user|
+    attributes_table do
+      row :name
+      row :email
+      row :sign_in_count
+      row :currently_signed_in do
+        if(user.current_sign_in_at.nil?)
+          "No"
+        else
+          "Yes, since #{pretty_format(user.current_sign_in_at)} from #{user.current_sign_in_ip}"
+        end
+      end
+      row :last_sign_in do
+        if(user.last_sign_in_at.nil?)
+          "Never"
+        else
+          "#{pretty_format(user.last_sign_in_at)} from #{user.last_sign_in_ip}"
+        end
+      end
+      row :public_key do
+        if user.public_key.nil?
+          status_tag("Missing", :error)
+        else
+          link_to 'Download Public Key', download_public_key_admin_user_path(user)
+        end
+      end
+      row :private_key do
+        if user.private_key.nil?
+          status_tag("Missing", :error)
+        else
+          link_to 'Download Private Key', download_private_key_admin_user_path(user)
+        end
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs 'User Information' do
+      f.input :email
+      f.input :name
+      unless f.object.persisted?
+        f.input :password
+        f.input :password_confirmation, :required => true
+      end
+      f.form_buffers.last # https://github.com/gregbell/active_admin/pull/965
+    end
+
+    f.buttons
+  end
+
+  member_action :download_public_key do
+    @user = User.find(params[:id])
+    
+    send_data @user.public_key, :filename => "#{@user.email}.pub" unless @user.public_key.nil?
+  end
+  member_action :download_private_key do
+    @user = User.find(params[:id])
+    
+    send_data @user.private_key, :filename => "#{@user.email}.key" unless @user.private_key.nil?
   end
 end
