e7651bf643c65d3d5a4b82bb1a5e927a95c61ac7
profmaad <prof.maad@lambda-bb.de>
2013-01-31 13:40:41 +0100

    Change users key field to text without length limit

---
+++ db/migrate/20130131123519_change_keys_in_users.rb
@@ -0,0 +1,11 @@
+class ChangeKeysInUsers < ActiveRecord::Migration
+  def up
+    change_column :users, :private_key, :text, :limit => nil
+    change_column :users, :public_key, :text, :limit => nil
+  end
+
+  def down
+    change_column :users, :private_key, :string
+    change_column :users, :public_key, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122172559) do
+ActiveRecord::Schema.define(:version => 20130131123519) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -112,8 +112,8 @@ ActiveRecord::Schema.define(:version => 20130122172559) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
-    t.string   "public_key"
-    t.string   "private_key"
+    t.text     "public_key"
+    t.text     "private_key"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
