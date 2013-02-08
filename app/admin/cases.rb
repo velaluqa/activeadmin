701b08535917d5b2d13d2a266acee37a9aea139b
profmaad <prof.maad@lambda-bb.de>
2013-02-08 15:03:52 +0100

    Only display delete case action when case was not yet answered (refs #310)

--- app/admin/cases.rb
+++ app/admin/cases.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Case do
 
   actions :index, :show, :destroy
+  config.clear_action_items! # get rid of the default action items, since we have to handle 'delete' on a case-by-case basis
 
   controller do
     load_and_authorize_resource :except => :index
@@ -24,7 +25,21 @@ ActiveAdmin.register Case do
       end
     end
 
-    default_actions
+    column '' do |resource|
+      # most of this is copied from activeadmin/lib/active_admin/views/index_as_table.rb#default_actions
+      # since we can't customize its behaviour
+      links = ''.html_safe
+      if controller.action_methods.include?('show')
+        links << link_to(I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link")
+      end
+      if controller.action_methods.include?('edit')
+        links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link")
+      end
+      if controller.action_methods.include?('destroy') and resource.form_answer.nil?
+        links << link_to(I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')}, :class => "member_link delete_link")
+      end
+      links
+    end
   end
 
   show do |c|
@@ -46,4 +61,13 @@ ActiveAdmin.register Case do
       end
     end
   end
+
+  action_item :only => :show do
+    # copied from activeadmin/lib/active_admin/resource/action_items.rb#add_default_action_items
+    if controller.action_methods.include?('destroy') and resource.form_answer.nil?
+      link_to(I18n.t('active_admin.delete_model', :model => active_admin_config.resource_label),
+              resource_path(resource),
+              :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')})
+    end
+  end 
 end
