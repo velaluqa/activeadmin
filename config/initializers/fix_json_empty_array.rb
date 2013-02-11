bd048f03817133243eb310347cabadbc72db894b
profmaad <prof.maad@lambda-bb.de>
2013-02-11 01:24:11 +0100

    Work around https://github.com/rails/rails/pull/8862

---
+++ config/initializers/fix_json_empty_array.rb
@@ -0,0 +1,23 @@
+# Rails "bug": https://github.com/rails/rails/pull/8862
+
+module ActionDispatch
+  class Request < Rack::Request
+    def deep_munge(hash)
+      hash.each do |k, v|
+        case v
+        when Array
+          if v.size > 0 && v.all?(&:nil?)
+            hash[k] = nil
+            next
+          end
+          v.grep(Hash) { |x| deep_munge(x) }
+          v.compact!
+        when Hash
+          deep_munge(v)
+        end
+      end
+
+      hash
+    end
+  end
+end
