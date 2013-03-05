78ed6d7c78c6ae635003309c8eb1403bc2531f6b
profmaad <prof.maad@lambda-bb.de>
2013-03-05 14:46:58 +0100

    Remove obsolete SessionPause model

--- app/models/session_pause.rb
+++
@@ -1,6 +0,0 @@
-class SessionPause < ActiveRecord::Base
-  attr_accessible :end, :reason, :session, :start, :last_case, :last_case_id
-
-  belongs_to :session
-  belongs_to :last_case, :class_name => 'Case'
-end
