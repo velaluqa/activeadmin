1bf5fb0f46416e90ff81a0899a166433d6f146cf
profmaad <prof.maad@lambda-bb.de>
2013-03-05 14:47:36 +0100

    Add object_changes column to versions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130304151126) do
+ActiveRecord::Schema.define(:version => 20130305122044) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -135,12 +135,13 @@ ActiveRecord::Schema.define(:version => 20130304151126) do
   add_index "validators_sessions", ["user_id", "session_id"], :name => "index_validators_sessions_on_user_id_and_session_id"
 
   create_table "versions", :force => true do |t|
-    t.string   "item_type",  :null => false
-    t.integer  "item_id",    :null => false
-    t.string   "event",      :null => false
+    t.string   "item_type",      :null => false
+    t.integer  "item_id",        :null => false
+    t.string   "event",          :null => false
     t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
+    t.text     "object_changes"
   end
 
   add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
