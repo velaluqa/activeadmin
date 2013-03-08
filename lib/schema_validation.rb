f111baf384abbc61610cf6d47c14a83f79c58bad
profmaad <prof.maad@lambda-bb.de>
2013-03-08 14:53:41 +0100

    Implement export (only CSV for now) (closes #369)

--- lib/schema_validation.rb
+++ lib/schema_validation.rb
@@ -1,2 +1,3 @@
 require 'schema_validation/session_validator'
 require 'schema_validation/form_validator'
+require 'schema_validation/export_validator'
---
+++ lib/schema_validation/export_validator.rb
@@ -0,0 +1,16 @@
+require 'kwalify'
+require 'yaml'
+
+module SchemaValidation
+  class ExportValidator < Kwalify::Validator
+    ## load schema definition
+    @@schema = YAML.load_file('lib/schema_validation/export.schema.yml') #HC
+
+    def initialize
+      super(@@schema)
+    end
+
+    # def validate_hook(value, rule, path, errors)
+    # end
+  end
+end
