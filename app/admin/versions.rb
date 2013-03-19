351ae1650fa3ba86e8283197d7202d63cb81392d
profmaad <prof.maad@lambda-bb.de>
2013-03-19 17:09:19 +0100

    Fix SQL statements for audit trail views for PostgreSQL

--- app/admin/versions.rb
+++ app/admin/versions.rb
@@ -16,21 +16,21 @@ ActiveAdmin.register Version do
 
       case params[:audit_trail_view_type]
       when 'case'
-        end_of_association_chain.where('item_type LIKE "Case" and item_id = ?', params[:audit_trail_view_id].to_i)
+        end_of_association_chain.where('item_type LIKE \'Case\' and item_id = ?', params[:audit_trail_view_id].to_i)
       when 'patient'
-        end_of_association_chain.where('item_type LIKE "Patient" and item_id = ?', params[:audit_trail_view_id].to_i)
+        end_of_association_chain.where('item_type LIKE \'Patient\' and item_id = ?', params[:audit_trail_view_id].to_i)
       when 'form'
-        end_of_association_chain.where('item_type LIKE "Form" and item_id = ?', params[:audit_trail_view_id].to_i)
+        end_of_association_chain.where('item_type LIKE \'Form\' and item_id = ?', params[:audit_trail_view_id].to_i)
       when 'role'
-        end_of_association_chain.where('item_type LIKE "Role" and item_id = ?', params[:audit_trail_view_id].to_i)
+        end_of_association_chain.where('item_type LIKE \'Role\' and item_id = ?', params[:audit_trail_view_id].to_i)
       when 'user'
-        end_of_association_chain.where('(item_type LIKE "User" and item_id = :user_id) or (item_type LIKE "Role" and item_id IN (SELECT id FROM roles WHERE roles.user_id = :user_id))',
+        end_of_association_chain.where('(item_type LIKE \'User\' and item_id = :user_id) or (item_type LIKE \'Role\' and item_id IN (SELECT id FROM roles WHERE roles.user_id = :user_id))',
                                        {:user_id => params[:audit_trail_view_id].to_i})
       when 'session'
-        end_of_association_chain.where('(item_type LIKE "Session" and item_id = :session_id) or (item_type LIKE "Case" and item_id IN (SELECT id FROM cases WHERE cases.session_id = :session_id)) or (item_type LIKE "Patient" and item_id IN (SELECT id FROM patients WHERE patients.session_id = :session_id)) or (item_type LIKE "Form" and item_id IN (SELECT id FROM forms WHERE forms.session_id = :session_id))',
+        end_of_association_chain.where('(item_type LIKE \'Session\' and item_id = :session_id) or (item_type LIKE \'Case\' and item_id IN (SELECT id FROM cases WHERE cases.session_id = :session_id)) or (item_type LIKE \'Patient\' and item_id IN (SELECT id FROM patients WHERE patients.session_id = :session_id)) or (item_type LIKE \'Form\' and item_id IN (SELECT id FROM forms WHERE forms.session_id = :session_id))',
                                        {:session_id => params[:audit_trail_view_id].to_i})
       when 'study'
-        end_of_association_chain.where('(item_type LIKE "Study" and item_id = :study_id) or (item_type LIKE "Session" and item_id IN (SELECT id FROM sessions WHERE sessions.study_id = :study_id)) or (item_type LIKE "Case" and item_id IN (SELECT id FROM cases WHERE cases.session_id IN (SELECT id FROM sessions WHERE sessions.study_id = :study_id))) or (item_type LIKE "Patient" and item_id IN (SELECT id FROM patients WHERE patients.session_id IN (SELECT id FROM sessions WHERE sessions.study_id = :study_id))) or (item_type LIKE "Form" and item_id IN (SELECT id FROM forms WHERE forms.session_id IN (SELECT id FROM sessions WHERE sessions.study_id = :study_id)))',
+        end_of_association_chain.where('(item_type LIKE \'Study\' and item_id = :study_id) or (item_type LIKE \'Session\' and item_id IN (SELECT id FROM sessions WHERE sessions.study_id = :study_id)) or (item_type LIKE \'Case\' and item_id IN (SELECT id FROM cases WHERE cases.session_id IN (SELECT id FROM sessions WHERE sessions.study_id = :study_id))) or (item_type LIKE \'Patient\' and item_id IN (SELECT id FROM patients WHERE patients.session_id IN (SELECT id FROM sessions WHERE sessions.study_id = :study_id))) or (item_type LIKE \'Form\' and item_id IN (SELECT id FROM forms WHERE forms.session_id IN (SELECT id FROM sessions WHERE sessions.study_id = :study_id)))',
                                        {:study_id => params[:audit_trail_view_id].to_i})
       else
         end_of_association_chain
