d70422297bc60388c398af67e765316d7151247b
profmaad <prof.maad@lambda-bb.de>
2013-04-05 18:05:10 +0200

    Fix config filtering, return entire config

--- lib/config_display_filters.rb
+++ lib/config_display_filters.rb
@@ -4,5 +4,7 @@ module ConfigDisplayFilters
       t['annotations_layout'] = '<cut for display>' unless t['annotations_layout'].nil?
       t['validation']['annotations_layout'] = '<cut for display>' unless (t['validation'].nil? or  t['validation']['annotations_layout'].nil?)
     end
+
+    return config
   end
 end
