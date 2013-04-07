0887caaffaf094ad881928ec589d525f4f0d120a
profmaad <prof.maad@lambda-bb.de>
2013-04-07 17:02:49 +0200

    Implement basic AA resource for Center

---
+++ app/admin/centers.rb
@@ -0,0 +1,54 @@
+require 'aa_customizable_default_actions'
+
+ActiveAdmin.register Center do
+
+  controller do
+    load_and_authorize_resource :except => :index
+    def scoped_collection
+      end_of_association_chain.accessible_by(current_ability)
+    end
+  end
+
+  index do
+    selectable_column
+    column :study
+    column :name
+    
+    customizable_default_actions do |resource|
+      resource.patients.empty? ? [] : [:destroy]
+    end
+  end
+
+  show do |center|
+    attributes_table do
+      row :study
+      row :name
+    end
+  end
+
+  form do |f|
+    f.inputs 'Details' do
+      f.input :study
+      f.input :name
+    end
+
+    f.buttons
+  end
+
+  # filters
+  filter :study
+  filter :name
+
+  action_item :only => :show do
+    # copied from activeadmin/lib/active_admin/resource/action_items.rb#add_default_action_items
+    if controller.action_methods.include?('destroy') and resource.patients.empty?
+      link_to(I18n.t('active_admin.delete_model', :model => active_admin_config.resource_label),
+              resource_path(resource),
+              :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')})
+    end
+  end 
+
+  action_item :only => :show do
+    link_to('Audit Trail', admin_versions_path(:audit_trail_view_type => 'center', :audit_trail_view_id => resource.id))
+  end
+end
