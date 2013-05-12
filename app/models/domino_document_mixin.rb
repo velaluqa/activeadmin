0af3be4834b0df09ac4b7918ad68008ee5df14da
profmaad <prof.maad@lambda-bb.de>
2013-05-12 17:48:31 +0200

    Rework domino integration callback from before_save to after_commit

--- app/models/domino_document_mixin.rb
+++ app/models/domino_document_mixin.rb
@@ -2,7 +2,7 @@ require 'domino_integration_client'
 
 module DominoDocument
   def self.included(base)
-    base.before_save :ensure_domino_document_exists
+    base.after_commit :ensure_domino_document_exists
   end
 
   def lotus_notes_url
@@ -10,11 +10,16 @@ module DominoDocument
   end
 
   def domino_document_needs_update?
-    not (self.changes.keys & domino_document_fields).empty?
+    not (self.previous_changes.keys & domino_document_fields).empty?
   end
 
   def ensure_domino_document_exists
+    # this is the case if this after_save callback was called for the very save action done is this callback
+    # if we wouldn't catch this, we could end up in an infinite loop
+    return true if self.previous_changes.include?('domino_unid')
+
     return true unless(domino_document_needs_update? or self.domino_unid.nil?)
+
     client = DominoIntegrationClient.new(self.study.domino_db_url, Rails.application.config.domino_integration_username, Rails.application.config.domino_integration_password)
     if client.nil?
       errors.add :name, 'Failed to communicate with the Domino server.'
@@ -23,11 +28,15 @@ module DominoDocument
 
     if self.domino_unid.nil?
       self.domino_unid = client.ensure_document_exists(domino_document_query, domino_document_form, domino_document_properties)
-      result = (not self.domino_unid.nil?)
+      result = (not self.domino_unid.nil?)      
     else
       result = client.update_document(self.domino_unid, domino_document_form, domino_document_properties)
     end
     errors.add :name, 'Failed to communicate with the Domino server.' if (result == false)
+
+    unless self.changes.empty?
+      result &&= self.save
+    end
 
     return result
   end
