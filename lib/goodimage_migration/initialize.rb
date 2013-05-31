ad7918eb4475732fcaa805f570c7d702babae5d8
profmaad <prof.maad@lambda-bb.de>
2013-05-31 18:13:51 +0200

    Factor out config parsing

--- lib/goodimage_migration/initialize.rb
+++ lib/goodimage_migration/initialize.rb
@@ -5,23 +5,29 @@ require 'pp'
 require 'goodimage_migration/models'
 
 module GoodImageMigration
-  def self.initialize
+  def self.migration_config
     environment = ENV['MIGRATION_ENV'] || Rails.env
-
-    DataMapper::Logger.new($stdout, :debug)
-
     config = YAML::load_file('config/goodimage_migration.yml')
+
     if(config.nil?)
       puts "No valid migration config found in config/goodimage_migration.yml, exiting."
-      return false
+      return nil
     elsif(config[environment].nil?)
       puts "Running in environment #{environment}, but no such environment is defined in the migration config, exiting."
-      return false
-    elsif(config[environment]['migration_db'].nil? or config[environment]['goodimage_db'].nil?)
+      return nil
+    end
+
+    return config[environment]
+  end
+
+  def self.initialize
+    DataMapper::Logger.new($stdout, :debug)
+
+    config = self.migration_config
+    if(config['migration_db'].nil? or config['goodimage_db'].nil?)
       puts "Running in environment #{environment}, but not all required databases (migration_db, goodimage_db) are defined in the config, exiting."
       return false
     end
-    config = config[environment]
 
     DataMapper.setup(:default, config['migration_db'])
     DataMapper.setup(:goodimage, config['goodimage_db'])
