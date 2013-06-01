f5b0c128422d91642e2a6a1a0afa3664a2f956a8
profmaad <prof.maad@lambda-bb.de>
2013-06-01 14:45:53 +0200

    Migrate functionality from create_initial_user script to rake task erica:create_appadmin[username]

--- create_initial_user.rb
+++
@@ -1,4 +0,0 @@
-initial_user = User.create(:name => 'Max Wolter', :username => 'profmaad', :password => 'change', :password_confirmation => 'change', :signature_password => 'signature', :signature_password_confirmation => 'signature')
-
-Role.create(:user => initial_user, :subject_type => nil, :subject_id => nil, :role => :manage)
-Role.create(:user => initial_user, :subject_type => nil, :subject_id => nil, :role => :image_manage)
--- create_initial_user.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bundle exec rails runner -e"$1" "eval(File.read 'create_initial_user.rb')"
