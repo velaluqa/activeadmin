794be4f16ce9811a3580f01bb3d813bc245d7cb4
profmaad <prof.maad@lambda-bb.de>
2013-06-04 13:32:00 +0200

    Rework images controller to allow directly uploading imagaes via HTTP, remove batch_create

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,31 +1,35 @@
 class ImagesController < ApplicationController
   before_filter :load_image_series
 
-  skip_before_filter :verify_authenticity_token, :only => [:create, :batch_create]
+  skip_before_filter :verify_authenticity_token, :only => [:create]
 
   def create
     authorize! :create, Image
 
+    if(@image_series.nil? or params[:file].blank?)
+      respond_to do |format|
+        format.json { render :json => {:success => false, :error_code => 1, :error => 'Missing parameters.'}, :status => :bad_request }
+      end
+      return
+    end
+
     image = Image.create(:image_series => @image_series)
-    
-    respond_to do |format|
-      format.json { render :json => {:success => !image.nil?, :image => image} }
-    end
-  end
-  def batch_create
-    authorize! :create, Image
+    unless(image.nil?)
+      begin
+        File.open(image.absolute_image_storage_path, 'w') do |target_file|
+          target_file.write(params[:file].read)
+        end
+      rescue Exception => e
+        image.destroy
 
-    count = params[:count]
-    
-    image_paths = []
-
-    count.times do
-      image = Image.create(:image_series => @image_series)
-      image_paths << image.image_storage_path unless image.nil?
+        respond_to do |format|
+          format.json { render :json => {:success => false, :error_code => 2, :error => 'Failed to write the uploaded file to the image storage.'}, :status => :internal_server_error }
+        end
+      end
     end
     
     respond_to do |format|
-      format.json { render :json => {:success => (image_paths.size == count), :image_paths => image_paths} }
+      format.json { render :json => {:success => (not image.nil? and image.file_is_present?), :image => image} }
     end
   end
 
