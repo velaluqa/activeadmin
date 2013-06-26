ba9a97880a1a5f2a03cf6827aa3c4b26e316fde6
profmaad <prof.maad@lambda-bb.de>
2013-06-26 15:37:01 +0200

    Return json hash from images search instead of AR objects

--- app/controllers/images_search_controller.rb
+++ app/controllers/images_search_controller.rb
@@ -13,9 +13,17 @@ class ImagesSearchController < ApplicationController
     end
 
     results = ImagesSearch.perform_search(term)
+    results_json = results.map do |result|
+      {
+        :text => result.text,
+        :id => result.result_id,
+        :type => result.result_type
+      }
+    end
+    pp results_json
 
     respond_to do |format|
-      format.json { render :json => {:success => true, :results => results} }
+      format.json { render :json => {:success => true, :results => results_json} }
     end
   end
 end
