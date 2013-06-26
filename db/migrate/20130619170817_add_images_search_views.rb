c451b5bf885e9f47c16476909c5d3abfb96c939b
profmaad <prof.maad@lambda-bb.de>
2013-06-26 16:45:41 +0200

    Fix view creation migration for PostgreSQL

--- db/migrate/20130619170817_add_images_search_views.rb
+++ db/migrate/20130619170817_add_images_search_views.rb
@@ -3,16 +3,16 @@ class AddImagesSearchViews < ActiveRecord::Migration
     puts ActiveRecord::Base.connection.adapter_name
     if(ActiveRecord::Base.connection.adapter_name == 'PostgreSQL')
       execute <<-SQL
-        create view studies_search as select studies.name as text, 'study_' || studies.id as result_id, 'study' as result_type from studies;
+        create view studies_search as select studies.name as text, 'study_' || studies.id as result_id, text 'study' as result_type from studies;
       SQL
       execute <<-SQL
-        create view centers_search as select centers.code || ' - ' || centers.name as text, 'center_' || centers.id as result_id, 'center' as result_type from centers;
+        create view centers_search as select centers.code || ' - ' || centers.name as text, 'center_' || centers.id as result_id, text 'center' as result_type from centers;
       SQL
       execute <<-SQL
-        create view patients_search as select centers.code || patients.subject_id as text, 'patient_' || patients.id as result_id, 'patient' as result_type from patients, centers where centers.id = patients.center_id;
+        create view patients_search as select centers.code || patients.subject_id as text, 'patient_' || patients.id as result_id, txt 'patient' as result_type from patients, centers where centers.id = patients.center_id;
       SQL
       execute <<-SQL
-        create view visits_search as select centers.code || patients.subject_id || '#' || visits.visit_number as text, 'visit_' || visits.id as result_id, 'visit' as result_type from visits,patients,centers where visits.patient_id = patients.id and patients.center_id = centers.id;
+        create view visits_search as select centers.code || patients.subject_id || '#' || visits.visit_number as text, 'visit_' || visits.id as result_id, text 'visit' as result_type from visits,patients,centers where visits.patient_id = patients.id and patients.center_id = centers.id;
       SQL
       
       execute <<-SQL
