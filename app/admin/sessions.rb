3157a4e9b73141df81b0605e679a3538205b0f38
profmaad <prof.maad@lambda-bb.de>
2013-07-02 18:29:01 +0200

    Add benchmark to session index (refs #566)

--- app/admin/sessions.rb
+++ app/admin/sessions.rb
@@ -46,9 +46,12 @@ ActiveAdmin.register Session do
     column :state, :sortable => :state do |session|
       session.state.to_s.camelize
     end
+    benchmark('Progress Column') do 
     column 'Progress' do |session|
       "#{session.case_list(:read).size} / #{session.case_list(:all).size}"
     end
+    end
+    benchmark('Config Column') do
     column :configuration do |session|
       if(session.has_configuration?)
         status_tag('Available', :ok)
@@ -56,13 +59,16 @@ ActiveAdmin.register Session do
         status_tag('Missing', :error)
       end
     end
+    end
 
+    benchmark('Actions Column') do
     customizable_default_actions do |session|
       except = []
       except << :destroy unless can? :destroy, session
       except << :edit unless can? :edit, session
       
       except
+    end
     end
   end
 
