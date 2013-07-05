dbd95ac66bd6cd7b0ae61525f06072b8ea7339d6
profmaad <prof.maad@lambda-bb.de>
2013-07-05 16:42:43 +0200

    Start implementing hierarchical image display

---
+++ app/admin/image_hierarchy.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register_page 'Image Hierarchy' do
+
+  content do
+    div(id: 'hierarchy_tree', 'data-url': nodes_admin_image_hierarchy_path) do
+      
+    end
+  end
+
+  page_action :nodes, :method => get do
+    raw_node_id = params[:node_id]
+    node = nil
+
+    if(raw_node_id.blank?)
+      node = :root
+    elsif(raw_node_id =~ /^center_([0-9]*)$/)
+      node_class = Center.find($1)
+    elsif(raw_node_id =~ /^patient_([0-9]*)$/)
+      node_class = Patient.find($1)
+    elsif(raw_node_id =~ /^visit_([0-9]*)$/)
+      node_class = Visit.find($1)
+    end
+
+    if(node.nil?)
+      respond_to do |format|
+        format.json { render :json => {'success' => false, 'data' => []} }
+      end
+      return
+    end
+
+  end
+end
