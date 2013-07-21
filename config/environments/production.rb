70aee7ed58eae8b45e85f0f9926a2d7e7cd1d7a0
profmaad <prof.maad@lambda-bb.de>
2013-07-21 14:34:03 +0200

    Add image_hierarchy.js.coffee to precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ StudyServer::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( forms_bootstrap_and_overrides.css )
+  config.assets.precompile += %w( forms_bootstrap_and_overrides.css image_hierarchy.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
