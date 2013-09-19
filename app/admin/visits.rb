fe0ed1c23cee54a1a17340e3453794ccd470b5dd
profmaad <prof.maad@lambda-bb.de>
2013-09-19 17:11:36 +0800

    Fix access to mQC for mqc role

--- app/admin/visits.rb
+++ app/admin/visits.rb
@@ -8,7 +8,7 @@ ActiveAdmin.register Visit do
 
   controller do
     load_and_authorize_resource :except => :index
-    skip_load_and_authorize_resource :only => [:assign_required_series, :assign_required_series_form, :tqc_results, :tqc, :tqc_form, :required_series_viewer, :required_series_dicom_metadata, :all_required_series_viewer]
+    skip_load_and_authorize_resource :only => [:assign_required_series, :assign_required_series_form, :tqc_results, :tqc, :tqc_form, :mqc_results, :mqc, :mqc_form, :required_series_viewer, :required_series_dicom_metadata, :all_required_series_viewer]
 
     def max_csv_records
       1_000_000
