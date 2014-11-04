51ed3bb3cb2fc92498d2ca70ba1bdafdd69b0fbe
profmaad <prof.maad@lambda-bb.de>
2014-11-04 18:11:32 +0800

    Implement a config file for ERICA remote,parse it in an initializer and add it to the Rails application config (refs #692)

---
+++ config/initializers/01_erica_remote.rb
@@ -0,0 +1,19 @@
+require 'yaml'
+
+erica_remote_config_path = Rails.root + 'config/erica_remote.yml'
+begin
+  erica_remote_config = YAML::load_file(erica_remote_config_path)
+  pp erica_remote_config
+
+  if(erica_remote_config['erica_remote'] and erica_remote_config['erica_remote']['enabled'])
+    Rails.application.config.erica_remote = erica_remote_config['erica_remote']
+    Rails.application.config.is_erica_remote = true
+  else
+    Rails.application.config.erica_remote = nil
+    Rails.application.config.is_erica_remote = false
+  end
+rescue Errno::ENOENT => e
+  puts "No ERICA remote config file found at '#{erica_remote_config_path}', not loading ERICA remote"
+  Rails.application.config.erica_remote = nil
+  Rails.application.config.is_erica_remote = false
+end
