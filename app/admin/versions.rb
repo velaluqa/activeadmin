c6999ca64bf06f0c1e956af64bd3f3af7f5c8599
profmaad <prof.maad@lambda-bb.de>
2014-11-05 18:41:38 +0800

    Sort item types alphabetically in audit trail filters

--- app/admin/versions.rb
+++ app/admin/versions.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register Version do
   actions :index, :show
 
   filter :created_at
-  filter :item_type, :as => :select, :collection => ['Case', 'Form', 'Patient', 'Role', 'Session', 'Study' , 'User', 'Center', 'ImageSeries', 'Image', 'Patient', 'Visit']
+  filter :item_type, :as => :select, :collection => ['Case', 'Form', 'Patient', 'Role', 'Session', 'Study' , 'User', 'Center', 'ImageSeries', 'Image', 'Patient', 'Visit'].sort
   filter :whodunnit, :label => 'User', :as => :select, :collection => proc { User.all }
   filter :event, :as => :check_boxes, :collection => ['create', 'update', 'destroy']
 
