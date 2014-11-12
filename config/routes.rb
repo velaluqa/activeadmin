06458a34e43401a0212e492e0d1c247c9bc2a05e
profmaad <prof.maad@lambda-bb.de>
2014-11-12 18:39:06 +0800

    Fix comments in activeadmin-mongoid and enable them

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# activeadmin-mongoid monkey-patches this method to always return false, thus disabling comments
+# at the moment, because of foobar, we can't patch this in activeadmin-mongoid without also updating activeadmin-mongoid (to the version that will only work with the new activeadmin generation)
+# long snafu short: it's currently easier to just re-monkey-patch-enable comments here than to fix activeadmin-mongoid... :(
+ActiveAdmin::Namespace # autoload
+class ActiveAdmin::Namespace
+  # Reenable comments
+  def comments?
+    allow_comments == true
+  end
+end
+
 StudyServer::Application.routes.draw do
   ActiveAdmin.routes(self)
 
