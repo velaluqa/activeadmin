ee007f3dacdd065c10c37ec141ee9e5659305397
profmaad <prof.maad@lambda-bb.de>
2014-11-24 15:41:04 +0800

    Fix copy/paste error in comment

--- app/controllers/erica_remote_controller.rb
+++ app/controllers/erica_remote_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class EricaRemoteController < ApplicationController
-  before_filter :skip_trackable # do not track requests to the WADO API as logins/logouts, because every single request would be counted as one login
+  before_filter :skip_trackable # do not track requests to the ERICA Remote API as logins/logouts, because every single request would be counted as one login
 
   # Force load models, so YAML::load can find them... *facepalm*
   Study
