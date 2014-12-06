0d912172fd6dc057317abbfcf948dce3f7f9c0f1
profmaad <prof.maad@lambda-bb.de>
2014-12-06 15:35:17 +0800

    Set sidekiq namespace based on erica settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,8 @@
+namespace = Rails.application.config.is_erica_remote ? 'erica_remote_' : 'erica_v2_'
+namespace += Rails.env
+
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://127.0.0.1:6379/0', :namespace => 'erica_v2_'+Rails.env }
+  config.redis = { :url => 'redis://127.0.0.1:6379/0', :namespace => namespace }
 
   config.server_middleware do |chain|
     chain.add Kiqstand::Middleware
@@ -7,5 +10,5 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://127.0.0.1:6379/0', :namespace => 'erica_v2_'+Rails.env }
+  config.redis = { :url => 'redis://127.0.0.1:6379/0', :namespace => namespace }
 end
