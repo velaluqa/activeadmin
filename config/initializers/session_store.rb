8b5b10c787971a65ab6d0b2a89f8b0eff07061c6
Arthur Andersen <aandersen@velalu.qa>
2015-03-23 14:04:44 +0100

    Use different session_store for ericaV2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-StudyServer::Application.config.session_store :cookie_store, key: '_Study Server_session'
+StudyServer::Application.config.session_store :cookie_store, key: 'ericaV2_Study Server_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
