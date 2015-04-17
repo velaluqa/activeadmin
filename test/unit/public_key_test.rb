ef095229835d6b190810ab8fe3833b7050f05d69
Arthur Andersen <aandersen@velalu.qa>
2015-04-17 10:17:49 +0200

    Remove empty autogenerated test files

--- test/unit/public_key_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PublicKeyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/session_pause_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SessionPauseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
