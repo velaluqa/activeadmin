47b85afef97e7d4bf9ca94d6219bad7e4e6eb5a7
Arthur Andersen <aandersen@velalu.qa>
2015-05-11 11:03:38 +0200

    Use first args elem as args if Array for Hash#pick
    This allows us to pass an array to Hash#pick or Hash#pick!.

--- config/initializers/monkeypatch_hash_pick.rb
+++ config/initializers/monkeypatch_hash_pick.rb
@@ -1,9 +1,11 @@
 class Hash
   def pick(*args)
+    args = args.first if args.first.is_a?(Array)
     select { |key, _| args.include(key) }
   end
 
   def pick!(*args)
+    args = args.first if args.first.is_a?(Array)
     select! { |key, _| args.include(key) }
   end
 end
