8272cf8a46a0c29832ddd435d6090297164dc142
Arthur Andersen <aandersen@velalu.qa>
2015-05-18 11:44:54 +0200

    Implement Remote class
    The Remote class holds all information about a specific remote and is
    responsible for the retrieval of the remote paths.

---
+++ lib/remote/remote.rb
@@ -0,0 +1,32 @@
+class Remote
+  attr_reader :url, :host
+  attr_reader :root, :data_dir, :form_config_dir, :session_config_dir,
+              :study_config_dir, :image_storage_dir
+
+  def initialize(options = {})
+    @url  = options.fetch(:url)
+    @host = options.fetch(:host)
+    retrieve_paths
+  end
+
+  private
+
+  def retrieve_paths
+    paths               = fetch_paths
+    @root               = Pathname.new(paths['root'])
+    @data_dir           = Pathname.new(paths['data_directory'])
+    @form_config_dir    = Pathname.new(paths['form_config_directory'])
+    @session_config_dir = Pathname.new(paths['session_config_directory'])
+    @study_config_dir   = Pathname.new(paths['study_config_directory'])
+    @image_storage_dir  = Pathname.new(paths['image_storage_directory'])
+  end
+
+  def fetch_paths
+    response = Net::HTTP.get_response URI.join(url, '/remote/paths.json')
+    case response
+    when Net::HTTPSuccess then JSON.parse(response.body)
+    else
+      fail "Failed to retrieve path information ERICA remote at #{uri}: #{response.message}"
+    end
+  end
+end
---
+++ spec/lib/remote/remote_spec.rb
@@ -0,0 +1,60 @@
+require 'remote/remote'
+
+RSpec.describe Remote do
+  before :each do
+    stub_request(:get, 'http://localhost:3001/remote/paths.json')
+      .to_return(
+        body: {
+          root: '/srv/ERICA/server',
+          data_directory: '/srv/ERICA/server/data',
+          form_config_directory: '/srv/ERICA/server/data/forms',
+          session_config_directory: '/srv/ERICA/server/data/sessions',
+          study_config_directory: '/srv/ERICA/server/data/studies',
+          image_storage_directory: '/images'
+        }.to_json
+      )
+    @remote = Remote.new(
+      url: 'http://localhost:3001',
+      host: 'root@10.0.0.1'
+    )
+  end
+
+  describe 'loading remote paths' do
+    describe '#root' do
+      it 'returns the correct Pathname' do
+        expect(@remote.root).to be_a Pathname
+        expect(@remote.root.to_s).to eq '/srv/ERICA/server'
+      end
+    end
+    describe '#data_dir' do
+      it 'returns the correct Pathname' do
+        expect(@remote.data_dir).to be_a Pathname
+        expect(@remote.data_dir.to_s).to eq '/srv/ERICA/server/data'
+      end
+    end
+    describe '#form_config_dir' do
+      it 'returns the correct Pathname' do
+        expect(@remote.form_config_dir).to be_a Pathname
+        expect(@remote.form_config_dir.to_s).to eq '/srv/ERICA/server/data/forms'
+      end
+    end
+    describe '#session_config_dir' do
+      it 'returns the correct Pathname' do
+        expect(@remote.session_config_dir).to be_a Pathname
+        expect(@remote.session_config_dir.to_s).to eq '/srv/ERICA/server/data/sessions'
+      end
+    end
+    describe '#study_config_dir' do
+      it 'returns the correct Pathname' do
+        expect(@remote.study_config_dir).to be_a Pathname
+        expect(@remote.study_config_dir.to_s).to eq '/srv/ERICA/server/data/studies'
+      end
+    end
+    describe '#image_storage_dir' do
+      it 'returns the correct Pathname' do
+        expect(@remote.image_storage_dir).to be_a Pathname
+        expect(@remote.image_storage_dir.to_s).to eq '/images'
+      end
+    end
+  end
+end
