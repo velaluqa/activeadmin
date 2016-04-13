0a5e400a131054bd71d217d24c0f10d630e04a82
Arthur Andersen <aandersen@velalu.qa>
2016-04-13 10:52:52 +0200

    Fully specify `Ability` constant as `::Ability`
    When changing the ApplicationController or the dependency `Ability`
    ActiveAdmin gets problems reloading those classes. All constants have to
    be fully specified.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -63,7 +63,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_ability
-    @current_ability ||= Ability.new(current_user)
+    @current_ability ||= ::Ability.new(current_user)
   end
 
   def after_sign_in_path_for(resource)
