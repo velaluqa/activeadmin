7e81e8603d1b2edc27898a727af42e2d13ce7dd3
Arthur Andersen <aandersen@velalu.qa>
2016-04-13 16:53:03 +0200

    Extend user factory with roles

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,5 +2,23 @@ FactoryGirl.define do
   factory :user do
     username { Faker::Internet.user_name }
     password 'password'
+
+    trait :changed_password do
+      password_changed_at DateTime.now
+    end
+
+    trait :with_role do
+      transient do
+        role :manage
+        subject_type nil
+      end
+
+      after :create do |user, evaluator|
+        create :role,
+               role: evaluator.role,
+               user: user,
+               subject_type: evaluator.subject_type
+      end
+    end
   end
 end
