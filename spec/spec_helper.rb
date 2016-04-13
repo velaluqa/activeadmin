73b213ca4e63448bb81237fd5a6a4b68245f008b
Arthur Andersen <aandersen@velalu.qa>
2016-04-13 16:52:34 +0200

    Add devise specific helpers and macros

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,6 +35,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, type: :controller
+  config.extend ControllerMacros, type: :controller
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,32 @@
+module ControllerMacros
+  def login_user(role = :manage, subject_type = nil)
+    before(:each) do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      sign_in FactoryGirl.create(:user, :changed_password, :with_role,
+                                 role: role,
+                                 subject_type: subject_type)
+    end
+  end
+
+  def login_user_with_abilities(&block)
+    before(:each) do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      sign_in FactoryGirl.create(:user, :changed_password)
+      ability = Object.new
+      ability.extend(CanCan::Ability)
+      ability.instance_eval(&block) if block_given?
+      expect(@controller.send(:active_admin_authorization))
+        .to receive(:cancan_ability)
+             .at_least(:once)
+             .and_return(ability)
+      expect(@controller)
+        .to receive(:current_ability)
+             .at_least(:once)
+             .and_return(ability)
+    end
+
+    after(:each) do
+      sign_out(:user)
+    end
+  end
+end
