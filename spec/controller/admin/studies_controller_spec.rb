08adc43f8de62745fcaea5b5d16540a984ade71a
Arthur Andersen <aandersen@velalu.qa>
2016-04-14 11:03:38 +0200

    Spec admin/studies#show

--- spec/controller/admin/studies_controller_spec.rb
+++ spec/controller/admin/studies_controller_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe Admin::StudiesController, type: :controller do
-  describe 'without current user' do
-    subject { get :index }
-    it { expect(subject.status).to eq 302 }
-    it { expect(subject).to redirect_to('/users/sign_in') }
-  end
 
   describe '#index' do
+    describe 'without current user' do
+      subject { get :index }
+      it { expect(subject.status).to eq 302 }
+      it { expect(subject).to redirect_to('/users/sign_in') }
+    end
+
     describe 'for authorized user' do
       login_user_with_abilities do
         can :read, Study
@@ -30,7 +31,46 @@ RSpec.describe Admin::StudiesController, type: :controller do
     end
   end
 
+  describe '#show' do
+    before(:each) do
+      @study = create(:study)
+    end
+
+    describe 'without current user' do
+      subject { get(:show, id: @study.id) }
+      it { expect(subject.status).to eq 302 }
+      it { expect(subject).to redirect_to('/users/sign_in') }
+    end
+
+    describe 'for authorized user' do
+      login_user_with_abilities do
+        can :read, Study
+      end
+
+      it 'succeeds' do
+        response = get(:show, id: @study.id)
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'for unauthorized user' do
+      login_user_with_abilities
+
+      it 'denies access' do
+        response = get(:show, id: @study.id)
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+
   describe '#new' do
+    describe 'without current user' do
+      subject { get(:new) }
+      it { expect(subject.status).to eq 302 }
+      it { expect(subject).to redirect_to('/users/sign_in') }
+    end
+
     describe 'for authorized user' do
       login_user_with_abilities do
         can :read, Study
@@ -38,7 +78,7 @@ RSpec.describe Admin::StudiesController, type: :controller do
       end
 
       it 'succeeds' do
-        response = get :new
+        response = get(:new)
         expect(response).to be_success
         expect(response).to have_http_status(200)
       end
@@ -50,7 +90,7 @@ RSpec.describe Admin::StudiesController, type: :controller do
       end
 
       it 'denies access' do
-        response = get :new
+        response = get(:new)
         expect(response).to have_http_status(:forbidden)
       end
     end
