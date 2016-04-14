2ef8a8e01d3fc2d763c378d686e615388b696887
Arthur Andersen <aandersen@velalu.qa>
2016-04-14 09:55:50 +0200

    Test index action of admin/studies

---
+++ spec/controller/admin/studies_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+RSpec.describe Admin::StudiesController, type: :controller do
+  describe 'without current user' do
+    subject { get :index }
+    it { expect(subject.status).to eq 302 }
+    it { expect(subject).to redirect_to('/users/sign_in') }
+  end
+
+  describe '#index' do
+    describe 'for authorized user' do
+      login_user_with_abilities do
+        can :read, Study
+      end
+
+      it 'succeeds' do
+        response = get :index
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'for unauthorized user' do
+      login_user_with_abilities
+
+      it 'denies access' do
+        response = get :index
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+end
