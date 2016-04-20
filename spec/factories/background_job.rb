bfa4cd084da3b6c1222ae2e8d3dcbd7c74d04724
Arthur Andersen <aandersen@velalu.qa>
2016-04-20 17:30:02 +0200

    Recreate BackgroundJob model with ActiveRecord

---
+++ spec/factories/background_job.rb
@@ -0,0 +1,29 @@
+FactoryGirl.define do
+  factory :background_job do
+    user
+    completed false
+
+    trait :complete do
+      completed true
+      progress 1.0
+      completed_at { DateTime.now }
+    end
+
+    trait :successful do
+      successful true
+    end
+
+    trait :failed do
+      successful false
+    end
+
+    trait :with_zipfile do
+      before(:create) do |job|
+        file = 'spec/tmp/background_job_results.zip'
+        FileUtils.touch(file)
+        job.results ||= {}
+        job.results['zipfile'] = file
+      end
+    end
+  end
+end
