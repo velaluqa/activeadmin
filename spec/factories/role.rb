117070f4c8ed2edcb5a292594e1d6133e969e47d
Arthur Andersen <aandersen@velalu.qa>
2016-05-02 11:21:02 +0200

    Add transient `with_permissions` for role factory

--- spec/factories/role.rb
+++ spec/factories/role.rb
@@ -1,7 +1,17 @@
 FactoryGirl.define do
   factory :role do
-    user
-    role :manage
-    subject_type 'System'
+    transient do
+      with_permissions({})
+    end
+
+    title { Faker::Lorem.words(2).join(' ') }
+
+    before(:create) do |role, evaluator|
+      evaluator.with_permissions.each_pair do |activity, subjects|
+        Array[subjects].flatten.each do |subject|
+          role.permissions << Permission.new(activity: activity, subject: subject)
+        end
+      end
+    end
   end
 end
