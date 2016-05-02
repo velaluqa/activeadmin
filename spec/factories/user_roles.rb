6607a5785d2b88ebc9a374f8550a8edf4a2bd27c
Arthur Andersen <aandersen@velalu.qa>
2016-05-02 11:21:35 +0200

    Add transient `with_permissions` for user_role factory

--- spec/factories/user_roles.rb
+++ spec/factories/user_roles.rb
@@ -1,10 +1,19 @@
 FactoryGirl.define do
   factory :user_role do
+    transient do
+      with_permissions({})
+    end
+
     user
     role
 
     # Scope is nil by default, which means this user_roles
     # role-permissions will be granted system-wide.
     scope_object { nil }
+
+    before(:create) do |user_role, evaluator|
+      user_role.role =
+        create(:role, with_permissions: evaluator.with_permissions)
+    end
   end
 end
