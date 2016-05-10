c60220c7c23f380efa931c40cc78eb2a7b06f97e
Arthur Andersen <aandersen@velalu.qa>
2016-05-10 13:31:09 +0200

    Refactor transient with_permissions for role factory

--- spec/factories/role.rb
+++ spec/factories/role.rb
@@ -7,9 +7,14 @@ FactoryGirl.define do
     title { Faker::Lorem.words(2).join(' ') }
 
     before(:create) do |role, evaluator|
-      evaluator.with_permissions.each_pair do |activity, subjects|
+      evaluator.with_permissions.each_pair do |subjects, activities|
         Array[subjects].flatten.each do |subject|
-          role.permissions << Permission.new(activity: activity, subject: subject)
+          Array[activities].flatten.each do |activity|
+            role.permissions << Permission.new(
+              activity: activity,
+              subject: subject
+            )
+          end
         end
       end
     end
