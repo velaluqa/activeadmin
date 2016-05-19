bace9aef85c195f72f92d931f591f10b69ff8a55
Arthur Andersen <aandersen@velalu.qa>
2016-05-19 12:08:37 +0200

    Add Role#add_permission

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -5,6 +5,52 @@ RSpec.describe Role do
     expect(new_role).not_to be_valid
   end
 
+  describe '#add_permission' do
+    describe 'for new permission' do
+      describe 'for new role' do
+        before(:each) do
+          @role = build(:role)
+          @role.add_permission(:read, Study)
+        end
+
+        it 'adds the new permission to the permissions array' do
+          expect(@role.allows?(:read, Study)).to be_truthy
+        end
+
+        it 'does not save the permission' do
+          expect(Permission.where(activity: :read, subject: Study).exists?).to be_falsy
+        end
+      end
+
+      describe 'for existing role' do
+        before(:each) do
+          @role = create(:role)
+          @role.add_permission(:read, Study)
+        end
+
+        it 'adds the new permission to the permissions array' do
+          expect(@role.allows?(:read, Study)).to be_truthy
+        end
+
+        it 'saves the permission' do
+          expect(@role.permissions.where(activity: :read, subject: Study).exists?).to be_truthy
+        end
+      end
+    end
+
+    describe 'for existing permission' do
+      before(:each) do
+        @role = create(:role, with_permissions: { Study => :read })
+        expect(@role.permissions.where(activity: :read, subject: Study).count).to eq 1
+        @role.add_permission(:read, Study)
+      end
+
+      it 'does nothing' do
+        expect(@role.permissions.where(activity: :read, subject: Study).count).to eq 1
+      end
+    end
+  end
+
   describe '#abilities' do
     before(:each) do
       @role = create(:role, with_permissions: {
