22d7f337299415851862cc8f5311e19d108d3a2a
Arthur Andersen <aandersen@velalu.qa>
2016-06-02 19:02:21 +0200

    Fix /admin/patients form

--- app/admin/patients.rb
+++ app/admin/patients.rb
@@ -116,7 +116,7 @@ ActiveAdmin.register Patient do
       f.input :subject_id, :hint => (f.object.persisted? ? 'Do not change this unless you are absolutely sure you know what you do. This can lead to problems in project management, because the Subject ID is used to identify patients across documents.' : '')
     end
 
-    f.buttons
+    f.actions
   end
 
   # filters
