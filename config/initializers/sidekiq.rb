b2e535e1e1d3b223fa865419e63599e15197c529
Arthur Andersen <aandersen@velalu.qa>
2016-07-13 16:06:39 +0200

    Remove kiqstand
    Kiqstand is needed for Sidekiq and Mongoid 3. With Mongoid 4 it became
    obsolete. But now that we have removed Mongo it should be obsolete anyway.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,10 +3,6 @@ namespace += Rails.env
 
 Sidekiq.configure_server do |config|
   config.redis = { :url => "redis://#{ENV['REDIS_URL'] || 'redis'}:6379/0", :namespace => namespace }
-
-  config.server_middleware do |chain|
-    chain.add Kiqstand::Middleware
-  end
 end
 
 Sidekiq.configure_client do |config|
