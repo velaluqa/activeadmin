d90b55ccc550e4fd81272bec63636ea64c57b323
Arthur Andersen <aandersen@velalu.qa>
2016-08-16 12:10:30 +0200

    Update devise configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,7 @@ StudyServer::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Default URL for Devise.
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
