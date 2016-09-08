316ab125517eb7121f39d7bbc1f4c1f52e52efb0
Arthur Andersen <aandersen@velalu.qa>
2016-09-08 13:03:31 +0200

    Configure sidekiq-scheduler to send throttled emails

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,7 +7,7 @@ Sidekiq.configure_server do |config|
   config.redis = { :url => "redis://#{ENV['REDIS_URL'] || 'redis'}:6379/0", :namespace => namespace }
 
   config.on(:startup) do
-    Sidekiq.schedule = YAML.load_file(File.expand_path("../../config/scheduler.yml", __FILE__))
+    Sidekiq.schedule = YAML.load_file(File.expand_path("../../../config/scheduler.yml", __FILE__))
     Sidekiq::Scheduler.reload_schedule!
   end
 end
