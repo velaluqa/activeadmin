0f6d0139ef23de36924d5702e2e66e9c265465d4
Arthur Andersen <aandersen@velalu.qa>
2016-09-09 16:19:39 +0200

    Configure rspec-sidekiq in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,17 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+RSpec::Sidekiq.configure do |config|
+  # Clears all job queues before each example
+  config.clear_all_enqueued_jobs = true
+
+  # Whether to use terminal colours when outputting messages
+  config.enable_terminal_colours = true
+
+  # Warn when jobs are not enqueued to Redis but to a job array
+  config.warn_when_jobs_not_processed_by_sidekiq = true
+end
+
 RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
