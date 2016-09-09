021d6b3f35b88a6aa7cadb95319ad5374a0c5162
Arthur Andersen <aandersen@velalu.qa>
2016-09-09 17:21:20 +0200

    Implement SendThrottledNotificationEmail worker

---
+++ app/workers/send_throttled_notification_email.rb
@@ -0,0 +1,20 @@
+class SendThrottledNotificationEmail
+  include Sidekiq::Worker
+
+  sidekiq_options(queue: :notifications, retry: 5)
+
+  # Invokes NotificationMailer within a Sidekiq job.
+  #
+  # @param [Integer] user_id The recipients user id
+  # @param [Integer] profile_id The triggered profile
+  # @param [Array<Integer>] notification_ids The notifications to send.
+  def perform(user_id, profile_id, notification_ids)
+    user = User.find_by(id: user_id)
+    profile = NotificationProfile.find_by(id: profile_id)
+    notifications = Notification.where(id: notification_ids)
+
+    NotificationMailer
+      .throttled_notification_email(user, profile, notifications)
+      .deliver_now
+  end
+end
