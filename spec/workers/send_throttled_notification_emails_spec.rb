97561877b0f75383177947019f2ef79944d8877b
Arthur Andersen <aandersen@velalu.qa>
2016-10-07 12:57:33 +0200

    Ensure static config value for worker spec

--- spec/workers/send_throttled_notification_emails_spec.rb
+++ spec/workers/send_throttled_notification_emails_spec.rb
@@ -5,6 +5,7 @@ describe SendThrottledNotificationEmails do
   it 'enqueues another send throttled notification email job' do
     SendThrottledNotificationEmails.perform_async('hourly')
     expect(SendThrottledNotificationEmails).to have_enqueued_sidekiq_job('hourly')
+    allow(Rails.application.config).to receive(:maximum_email_throttling_delay).and_return(30*24*60*60)
   end
 
   context 'with existing throttled notifications' do
@@ -18,14 +19,14 @@ describe SendThrottledNotificationEmails do
       @notification3 = create(:notification, notification_profile: @profile1, user: @user3, email_sent_at: 1.minute.ago, created_at: 2.minutes.ago)
     end
 
-    it 'enqueues pending emailing jobs' do
+    it 'enqueues pending emailing jobs for `hourly`' do
       SendThrottledNotificationEmails.new.perform(60*60)
       expect(SendThrottledNotificationEmail).not_to have_enqueued_sidekiq_job(@user1.id, @profile1.id, [@notification1.id])
       expect(SendThrottledNotificationEmail).to have_enqueued_sidekiq_job(@user2.id, @profile1.id, [@notification2.id])
       expect(SendThrottledNotificationEmail).not_to have_enqueued_sidekiq_job(@user3.id, @profile1.id, [@notification3.id])
     end
 
-    it 'enqueues pending emailing jobs' do
+    it 'enqueues pending emailing jobs for `daily`' do
       SendThrottledNotificationEmails.new.perform(24*60*60)
       expect(SendThrottledNotificationEmail).to have_enqueued_sidekiq_job(@user1.id, @profile1.id, [@notification1.id])
       expect(SendThrottledNotificationEmail).not_to have_enqueued_sidekiq_job(@user2.id, @profile1.id, [@notification2.id])
