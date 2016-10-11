8a02b7fe3f8ac0160f94599a778e2a74aa99c01f
Arthur Andersen <aandersen@velalu.qa>
2016-10-11 18:00:30 +0200

    Add `transactional_spec` around spec filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -89,6 +89,12 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
+  config.around(:each, transactional_spec: true) do |example|
+    DatabaseCleaner.strategy = :deletion
+    example.run
+    DatabaseCleaner.strategy = :transaction
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
