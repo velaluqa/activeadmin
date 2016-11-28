60c88295d334a0d786951fe304759838b4f732bf
Arthur Andersen <aandersen@velalu.qa>
2016-11-28 13:59:34 +0100

    Put email_template field to the bottom of the form

--- app/admin/notification_profiles.rb
+++ app/admin/notification_profiles.rb
@@ -97,8 +97,8 @@ ActiveAdmin.register NotificationProfile do
       f.input :users, type: :select, multiple: true, collection: User.all, input_html: { class: 'initialize-select2' }
       f.input :roles, type: :select, multiple: true, collection: Role.all, input_html: { class: 'initialize-select2' }
       f.input :only_authorized_recipients
-      f.input :email_template_id, as: :select, collection: EmailTemplate.where(email_type: 'NotificationProfile'), input_html: { class: 'initialize-select2' }
       f.input :maximum_email_throttling_delay, as: :select, collection: Email.allowed_throttling_delays, input_html: { class: 'initialize-select2' }
+      f.input :email_template_id, as: :select, collection: EmailTemplate.where(email_type: 'NotificationProfile'), input_html: { class: 'initialize-select2' }
     end
 
     f.actions
