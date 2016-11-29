668a5d8078f8631133b5982d58ebc5e624379ec7
Arthur Andersen <aandersen@velalu.qa>
2016-11-29 12:16:09 +0100

    Add custom page /admin/help

---
+++ app/admin/help.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register_page 'Help' do
+  content do
+    render partial: 'index'
+  end
+
+  controller do
+    def authorize_access!
+    end
+  end
+
+  Dir[Rails.root.join('app/views/admin/help/*.md')].each do |help_page|
+    name = File.basename(help_page, '.*')
+    next if name[0] == '_'
+    page_action name.to_sym, method: :get do
+      render(
+        file: 'app/views/admin/help/_document',
+        locals: { document: name.to_sym },
+        layout: true
+      )
+    end
+  end
+end
