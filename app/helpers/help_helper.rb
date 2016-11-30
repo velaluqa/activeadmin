fe5914fa4acef7eeb6ef7e55274fbc63f9eb1e1b
Arthur Andersen <aandersen@velalu.qa>
2016-11-30 18:34:48 +0100

    Remove heading tree extraction from help_helper

--- app/helpers/help_helper.rb
+++ app/helpers/help_helper.rb
@@ -2,7 +2,6 @@ module HelpHelper
   def help_document(doc_name)
     doc = render file: "app/views/admin/help/#{doc_name}"
     headings = []
-    paths = []
     path = {}
     last_level = 0
     head_start = doc.index(/<h(\d)>/)
@@ -16,7 +15,6 @@ module HelpHelper
       else
         path[level] = title
       end
-      paths.push(path.deep_dup)
 
       before = doc[0...head_start]
       after = doc[head_start + "<h#{level}>".length...-1]
@@ -27,24 +25,7 @@ module HelpHelper
       head_end = doc.index(/<\/h#{level}>/, head_start)
       head_start = doc.index(/<h(\d)>/, head_end)
     end
-    [doc, paths]
-  end
-
-  def extract_help_headings(paths)
-    paths.each do |path|
-      max_level = path.keys.sort.max
-      path[:anchor] = heading_id_for_path(path)
-      path[:title] = path[max_level]
-    end
-    extract_help_groups(2, paths)
-  end
-
-  def extract_help_groups(level, paths)
-    paths.select { |path| path[level] && path[level+1].nil? }.map do |path|
-      grouped = paths.select { |spath| spath[level] == path[:title] && spath[level+1] }
-      path[:sub_headings] = extract_help_groups(level+1, grouped)
-      path
-    end
+    doc
   end
 
   private
