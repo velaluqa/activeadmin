c65a6a1ba83f4af91009183924f8c4910ee9e53d
Arthur Andersen <aandersen@velalu.qa>
2016-12-20 10:46:22 +0100

    Add `Report::create` to create `Report` instance from `type` String

--- lib/report.rb
+++ lib/report.rb
@@ -1,6 +1,18 @@
 require 'report/overview'
 
 module Report
+  class UnknownReportType < StandardError; end
+
+  def self.create(options)
+    type = options[:type]
+    params = options[:params].merge(user: options[:user])
+
+    case type
+    when 'overview' then Report::Overview.new(params)
+    else raise UnknownReportType, "Unknown type: #{type}"
+    end
+  end
+
   def self.mappings
     @mappings ||= {
       Visit => { 'state' => Visit::STATE_SYMS }
