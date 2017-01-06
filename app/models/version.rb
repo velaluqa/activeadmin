ffd4d430d7413e4d10c1b50563f5fb016ea3a626
Arthur Andersen <aandersen@velalu.qa>
2017-01-06 19:06:00 +0100

    Add `Version::of_study_resource`

--- app/models/version.rb
+++ app/models/version.rb
@@ -23,4 +23,54 @@
 #
 class Version < PaperTrail::Version
   has_many :notifications
+
+
+  class << self
+    # Scopes all versions for a given `study` and `item_type`.
+    def of_study_resource(study, resource_type)
+      study = Study.find(study) if study.is_a?(Fixnum)
+      rel = Version.where(item_type: resource_type)
+      case resource_type
+      when 'Patient' then patient_query(rel, study)
+      when 'Visit' then visit_query(rel, study)
+      when 'ImageSeries' then image_series_query(rel, study)
+      else rel
+      end
+    end
+
+    def after(date, options = {})
+      rel = where('"versions"."created_at" > ?', date)
+      rel = rel.where(options) unless options.empty?
+      rel.order('"versions"."id" ASC')
+    end
+
+    def before(date, options = {})
+      rel = where('"versions"."created_at" < ?', date)
+      rel = rel.where(options) unless options.empty?
+      rel.order('"versions"."id" DESC')
+    end
+
+    private
+
+    def patient_query(rel, study)
+      rel.where(<<QUERY)
+   (object_changes -> 'center_id' ->> 1)::integer IN (#{study.centers.select(:id).to_sql})
+OR (object ->> 'center_id')::integer IN (#{study.centers.select(:id).to_sql})
+QUERY
+    end
+
+    def visit_query(rel, study)
+      rel.where(<<QUERY)
+   (object_changes -> 'patient_id' ->> 1)::integer IN (#{study.patients.select(:id).to_sql})
+OR (object ->> 'patient_id')::integer IN (#{study.patients.select(:id).to_sql})
+QUERY
+    end
+
+    def image_series_query(rel, study)
+      rel.where(<<QUERY)
+   (object_changes -> 'patient_id' ->> 1)::integer IN (#{study.patients.select(:id).to_sql})
+OR (object ->> 'patient_id')::integer IN (#{study.patients.select(:id).to_sql})
+QUERY
+    end
+  end
 end
--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -1,3 +1,42 @@
 RSpec.describe Version do
   it { should have_many(:notifications) }
+
+  describe 'scope ::of_study_resource' do
+    before(:each) do
+      @study = create(:study)
+      @study_version = Version.last
+      @center = create(:center, study: @study)
+      @center_version = Version.last
+      @patient = create(:patient, center: @center)
+      @patient_version = Version.last
+      @visit = create(:visit, patient: @patient)
+      @visit_version = Version.last
+      @image_series = create(:image_series, patient: @patient)
+      @image_series_version = Version.last
+    end
+
+    describe 'with resource `Patient`' do
+      it 'includes patient version' do
+        expect(Version.of_study_resource(@study, 'Patient')).to include(@patient_version)
+        expect(Version.of_study_resource(@study, 'Patient')).not_to include(@visit_version)
+        expect(Version.of_study_resource(@study, 'Patient')).not_to include(@image_series_version)
+      end
+    end
+
+    describe 'with resource `Visit`' do
+      it 'includes visit version' do
+        expect(Version.of_study_resource(@study, 'Visit')).not_to include(@patient_version)
+        expect(Version.of_study_resource(@study, 'Visit')).to include(@visit_version)
+        expect(Version.of_study_resource(@study, 'Visit')).not_to include(@image_series_version)
+      end
+    end
+
+    describe 'with resource `ImageSeries`' do
+      it 'includes image_series version' do
+        expect(Version.of_study_resource(@study, 'ImageSeries')).not_to include(@patient_version)
+        expect(Version.of_study_resource(@study, 'ImageSeries')).not_to include(@visit_version)
+        expect(Version.of_study_resource(@study, 'ImageSeries')).to include(@image_series_version)
+      end
+    end
+  end
 end
