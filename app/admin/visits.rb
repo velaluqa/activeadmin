0b9299c0472198416efd9c4af495a9eab3c474b8
Arthur Andersen <aandersen@velalu.qa>
2017-01-12 16:10:40 +0100

    Only use patient_id param if existing in params hash

--- app/admin/visits.rb
+++ app/admin/visits.rb
@@ -208,7 +208,7 @@ ActiveAdmin.register Visit do
                   else
                     nil
                   end
-    f.object.patient_id = params[:patient_id]
+    f.object.patient_id = params[:patient_id] if params.key?(:patient_id)
 
     f.inputs 'Details' do
       f.input :patient, :collection => (f.object.persisted? ? f.object.study.patients : patients), :include_blank => (not f.object.persisted?)
