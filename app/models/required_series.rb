2887b573b6bb46808de1ea79d947d2424bd3f8a5
Arthur Andersen <aandersen@velalu.qa>
2017-01-12 15:22:06 +0100

    Add `RequiredSeries` `::count_for_study` and `::grouped_count_for_study`

--- app/models/required_series.rb
+++ app/models/required_series.rb
@@ -46,6 +46,50 @@ class RequiredSeries < ActiveRecord::Base
     return RequiredSeries.new(visit, id[1])
   end
 
+  def self.count_for_study(study_id)
+    res = ActiveRecord::Base.connection.execute(<<QUERY)
+SELECT
+  COUNT(image_series_id) AS count
+FROM visits
+JOIN json_each(visits.required_series::json) visits_required_series_hash ON true
+JOIN json_to_record(visits_required_series_hash.value)
+  AS visits_required_series(
+    image_series_id int,
+    tqc_state int
+) ON true
+INNER JOIN patients p ON p.id = visits.patient_id
+INNER JOIN centers c ON c.id = p.center_id
+WHERE
+  image_series_id IS NOT NULL AND tqc_state IS NOT NULL
+QUERY
+    res.first['count'].to_i
+  end
+
+  def self.grouped_count_for_study(study_id, group)
+    res = ActiveRecord::Base.connection.execute(<<QUERY)
+SELECT
+  tqc_state AS group,
+  COUNT(image_series_id) AS count
+FROM visits
+JOIN json_each(visits.required_series::json) visits_required_series_hash ON true
+JOIN json_to_record(visits_required_series_hash.value)
+  AS visits_required_series(
+    image_series_id int,
+    tqc_state int
+) ON true
+INNER JOIN patients p ON p.id = visits.patient_id
+INNER JOIN centers c ON c.id = p.center_id
+WHERE
+  image_series_id IS NOT NULL
+  AND tqc_state IS NOT NULL
+GROUP BY
+  tqc_state
+QUERY
+    res
+      .map { |result| [result['group'], result['count'].to_i] }
+      .to_h
+  end
+
   def initialize(visit, name)
     @visit = visit
     @name = name
