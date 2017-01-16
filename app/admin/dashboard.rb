1163961a065e82522b5491e76045f6e3e6f60481
Arthur Andersen <aandersen@velalu.qa>
2017-01-16 13:33:38 +0100

    Fix i18n procs in admin/dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register_page 'Dashboard' do
-  menu priority: 1, label: -> { I18n.t('active_admin.dashboard') }
+  menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
-  content title: -> { I18n.t('active_admin.dashboard') } do
+  content title: proc { I18n.t('active_admin.dashboard') } do
     render file: 'admin/dashboard/index'
   end
 end
