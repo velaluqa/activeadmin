fad7053cedf79967de2612ce4a6865d4d60bf702
Arthur Andersen <aandersen@velalu.qa>
2017-01-16 13:33:57 +0100

    Add admin/dashboard/save end-point saving given config

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,4 +4,20 @@ ActiveAdmin.register_page 'Dashboard' do
   content title: proc { I18n.t('active_admin.dashboard') } do
     render file: 'admin/dashboard/index'
   end
+
+  controller do
+    def authorize_access!
+      # Permissions are handled internally.
+      # The components retrieving report information take permissions
+      # into account.
+    end
+  end
+
+  page_action :save, method: :post do
+    current_user.dashboard_configuration = {
+      general: params[:config]
+    }
+    current_user.save
+    render json: {}
+  end
 end
