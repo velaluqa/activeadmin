21c1db887884381934b6221ea18636806dc4aa26
Arthur Andersen <aandersen@velalu.qa>
2017-01-16 11:57:46 +0100

    Clean up admin/dashboard.rb according to style guide

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,7 +1,7 @@
-ActiveAdmin.register_page "Dashboard" do
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+ActiveAdmin.register_page 'Dashboard' do
+  menu priority: 1, label: -> { I18n.t('active_admin.dashboard') }
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
+  content title: -> { I18n.t('active_admin.dashboard') } do
     render file: 'admin/dashboard/index'
-  end # content
+  end
 end
