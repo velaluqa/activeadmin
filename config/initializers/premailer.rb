22c637e8e13ba0ab1ddec178ac8a931238a5695b
Arthur Andersen <aandersen@velalu.qa>
2017-02-02 16:40:31 +0100

    Initialize premailer

--- config/initializers/premailer.rb
+++ config/initializers/premailer.rb
@@ -1,6 +1,1 @@
-Premailer::Rails.config.merge!(
-  preserve_styles: true,
-  remove_ids: true,
-  generate_text_part: false,
-  adapter: 'nokogiri'
-)
+Premailer::Rails.config.merge!(preserve_styles: true, remove_ids: true)
