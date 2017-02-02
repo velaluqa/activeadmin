d56c78f11e0a90a4374aa0d18cc1ea00a54e642b
Arthur Andersen <aandersen@velalu.qa>
2017-02-02 11:16:21 +0100

    Allow center id preset via url params for /admin/patients/new

--- app/admin/patients.rb
+++ app/admin/patients.rb
@@ -110,7 +110,7 @@ ActiveAdmin.register Patient do
 
   form do |f|
     centers = (session[:selected_study_id].nil? ? Center.accessible_by(current_ability) : Study.find(session[:selected_study_id]).centers.accessible_by(current_ability))
-
+    f.object.center_id = params[:center_id] if params.key?(:center_id)
     f.inputs 'Details' do
       f.input :center, :collection => (f.object.persisted? ? f.object.study.centers : centers), :include_blank => (not f.object.persisted?)
       f.input :subject_id, :hint => (f.object.persisted? ? 'Do not change this unless you are absolutely sure you know what you do. This can lead to problems in project management, because the Subject ID is used to identify patients across documents.' : '')
