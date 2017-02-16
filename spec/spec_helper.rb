bc90db81fca621385fb1c864806d3b7b2e45384e
Arthur Andersen <aandersen@velalu.qa>
2017-02-16 16:29:29 +0100

    Configure poltergeist as javascript driver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ Dir.glob('spec/features/steps/**/*_steps.rb') { |f| load f, true }
 require 'capybara/rails'
 
 require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 Capybara.default_driver = :poltergeist
 
 require 'yarjuf'
