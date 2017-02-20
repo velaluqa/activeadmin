64f5c7fa9dc742bb4c2add50d329653e7674b8b9
Arthur Andersen <aandersen@velalu.qa>
2017-02-20 17:19:52 +0100

    Configure Formtastic FormBuilder class finders

---
+++ config/initializers/formtastic.rb
@@ -0,0 +1,3 @@
+# Taken from: http://bit.ly/1F9QtKc
+Formtastic::FormBuilder.action_class_finder = Formtastic::ActionClassFinder
+Formtastic::FormBuilder.input_class_finder = Formtastic::InputClassFinder
