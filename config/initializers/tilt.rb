2bbe413d6d7b21f73bba55c28c21ba5a5c073b87
Arthur Andersen <aandersen@velalu.qa>
2017-04-24 12:01:22 +0200

    Require `tilt/coffee` in initializer at app start
    This gets rid of messages like:
    
    ```
    WARN: tilt autoloading 'tilt/coffee' in a non thread-safe way; explicit require 'tilt/coffee' suggested
    ```

---
+++ config/initializers/tilt.rb
@@ -0,0 +1,4 @@
+# Requiring `titl/coffee` here gets rid of these annoying messages:
+#
+#   WARN: tilt autoloading 'tilt/coffee' in a non thread-safe way; explicit require 'tilt/coffee' suggested
+require 'tilt/coffee'
