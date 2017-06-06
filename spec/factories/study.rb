e0655db2903e15fb61bb75fbe549db9cf0559ec8
Arthur Andersen <aandersen@velalu.qa>
2017-06-06 12:22:37 +0200

    Add transient attribute `configuration` to study factory
    This allows quick creation of configured studies.

--- spec/factories/study.rb
+++ spec/factories/study.rb
@@ -2,6 +2,10 @@ FactoryGirl.define do
   factory :study do
     sequence(:name) { |n| "Study #{n}" }
 
+    transient do
+      configuration(nil)
+    end
+
     trait :production do
       state Study.state_sym_to_int(:production)
     end
@@ -17,6 +21,17 @@ FactoryGirl.define do
 
       after :create do |study, evaluator|
         create_list :center, evaluator.centers_count, study: study
+      end
+    end
+
+    after(:create) do |study, evaluator|
+      if evaluator.configuration.is_a?(String)
+        tempfile = Tempfile.new('test.yml')
+        tempfile.write(evaluator.configuration)
+        tempfile.close
+        repo = GitConfigRepository.new
+        repo.update_config_file(study.relative_config_file_path, tempfile, nil, "New configuration file for study #{study.id}")
+        tempfile.unlink
       end
     end
   end
