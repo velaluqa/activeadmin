26b43fa10e493396960cb6f2c9f19c382a073302
Arthur Andersen <aandersen@velalu.qa>
2017-06-14 16:22:30 +0200

    Add `RequiredSeries` schema information comment

--- app/models/required_series.rb
+++ app/models/required_series.rb
@@ -1,3 +1,33 @@
+# ## Schema Information
+#
+# Table name: `required_series`
+#
+# ### Columns
+#
+# Name                   | Type               | Attributes
+# ---------------------- | ------------------ | ---------------------------
+# **`created_at`**       | `datetime`         | `not null`
+# **`domino_unid`**      | `string`           |
+# **`id`**               | `integer`          | `not null, primary key`
+# **`image_series_id`**  | `integer`          |
+# **`name`**             | `string`           | `not null`
+# **`tqc_comment`**      | `text`             |
+# **`tqc_date`**         | `datetime`         |
+# **`tqc_results`**      | `jsonb`            |
+# **`tqc_state`**        | `integer`          |
+# **`tqc_user_id`**      | `integer`          |
+# **`tqc_version`**      | `string`           |
+# **`updated_at`**       | `datetime`         | `not null`
+# **`visit_id`**         | `integer`          | `not null`
+#
+# ### Indexes
+#
+# * `index_required_series_on_image_series_id`:
+#     * **`image_series_id`**
+# * `index_required_series_on_visit_id_and_name` (_unique_):
+#     * **`visit_id`**
+#     * **`name`**
+#
 class RequiredSeries < ActiveRecord::Base
   belongs_to :visit
   belongs_to :image_series
