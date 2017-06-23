3bd5776aef1e720fdb08bd643d41d8e025d82ac2
Arthur Andersen <aandersen@velalu.qa>
2017-06-23 15:23:01 +0200

    Use latest required series version to get the object from

--- lib/migration/migrate_required_series_changes.rb
+++ lib/migration/migrate_required_series_changes.rb
@@ -5,22 +5,44 @@ module Migration
         RequiredSeries.skip_callback(:save, :after, :update_image_series_state)
         versions.each do |version|
           required_series_changes(version).each do |mapping|
-            required_series = RequiredSeries.where(visit_id: mapping['visit_id'], name: mapping['name']).first
-            Version.create(
-              item_type: 'RequiredSeries',
-              item_id: required_series.id,
-              event: 'update',
-              object: JSON.parse(required_series.to_json),
-              object_changes: mapping['changes'],
-              created_at: version.created_at,
-              study_id: version.study_id
-            )
-            attributes = mapping['changes'].map { |col, (_, new)| [col, new] }.to_h
-            required_series.update_attributes(attributes)
-            required_series.save!
+            create_required_series_version(version, **mapping)
           end
         end
         RequiredSeries.set_callback(:save, :after, :update_image_series_state)
+      end
+
+      def create_required_series_version(visit_version, visit_id:, name:, changes:)
+        latest_version = find_latest_required_series_version(visit_id, name)
+        Version.create(
+          item_type: 'RequiredSeries',
+          item_id: latest_version.item_id,
+          event: 'update',
+          object: latest_version.complete_attributes,
+          object_changes: changes,
+          created_at: visit_version.created_at,
+          study_id: visit_version.study_id
+        )
+        required_series = RequiredSeries.where(id: latest_version.item_id).first
+        return if required_series.nil?
+        attributes = changes.transform_values { |_, new| new }
+        required_series.update_attributes(attributes)
+        required_series.save!
+      end
+
+      def find_latest_required_series_version(visit_id, name)
+        Version
+          .where(item_type: 'RequiredSeries')
+          .where(<<CLAUSE.strip_heredoc, visit_id: visit_id, name: name)
+            (
+              ((object ->> 'name') LIKE :name) OR
+              ((object_changes #>> '{name,1}') LIKE :name)
+            ) AND (
+              ((object ->> 'visit_id')::int = :visit_id) OR
+              ((object_changes #>> '{visit_id,1}')::int = :visit_id)
+            )
+CLAUSE
+          .order(:created_at, :id)
+          .last
       end
 
       def versions
@@ -38,9 +60,9 @@ CLAUSE
         was, becomes = visit_version.complete_changes['required_series']
         (was.keys + becomes.keys).uniq.map do |required_series_name|
           {
-            'visit_id' => visit_version.item_id,
-            'name' => required_series_name,
-            'changes' => RequiredSeries.columns.map do |column|
+            visit_id: visit_version.item_id,
+            name: required_series_name,
+            changes: RequiredSeries.columns.map do |column|
               was_value = was.andand[required_series_name].andand[column.name]
               becomes_value = becomes.andand[required_series_name].andand[column.name]
               next if was_value == becomes_value
--- spec/lib/migration/migrate_required_series_changes_spec.rb
+++ spec/lib/migration/migrate_required_series_changes_spec.rb
@@ -7,19 +7,27 @@ describe Migration::MigrateRequiredSeriesChanges, paper_trail: false do
         let!(:visit) { create(:visit) }
         let!(:required_series) { create(:required_series, visit: visit, name: 'SPECT_1') }
 
-        let!(:version) do
+        let!(:version1) do
+          Version.create!(
+            item_type: 'RequiredSeries',
+            item_id: required_series.id,
+            event: 'create',
+            object_changes: {
+              'id' => [nil, required_series.id],
+              'visit_id' => [nil, visit.id],
+              'name' => [nil, 'SPECT_1'],
+              'created_at' => [nil, required_series.created_at]
+            }
+          )
+        end
+        let!(:version2) do
           Version.create!(
             item_type: 'Visit',
             item_id: visit.id,
             event: 'update',
             object_changes: {
               'required_series' => [
-                {
-                  'SPECT_1' => {
-                    image_series_id: nil,
-                    tqc_state: nil
-                  },
-                }, {
+                {}, {
                   'SPECT_1' => {
                     image_series_id: "5",
                     tqc_state: 0
@@ -48,9 +56,39 @@ describe Migration::MigrateRequiredSeriesChanges, paper_trail: false do
         let!(:visit) { create(:visit) }
         let!(:required_series) { create(:required_series, visit: visit, name: 'SPECT_1') }
         let!(:user) { create(:user) }
-        let!(:date) { DateTime.now }
+        let!(:date) { DateTime.new(2017, 6, 3, 12, 34, 17, 344) }
 
-        let!(:version) do
+        let!(:version1) do
+          Version.create!(
+            item_type: 'RequiredSeries',
+            item_id: required_series.id,
+            event: 'create',
+            object_changes: {
+              'id' => [nil, required_series.id],
+              'visit_id' => [nil, visit.id],
+              'name' => [nil, 'SPECT_1'],
+              'created_at' => [nil, required_series.created_at]
+            }
+          )
+        end
+        let!(:version2) do
+          Version.create!(
+            item_type: 'RequiredSeries',
+            item_id: required_series.id,
+            event: 'create',
+            object: {
+              'id' => required_series.id,
+              'visit_id' => visit.id,
+              'name' => 'SPECT_1',
+              'created_at' => required_series.created_at
+            },
+            object_changes: {
+              'image_series_id' => [nil, 5],
+              'tqc_state' => [nil, 'pending']
+            }
+          )
+        end
+        let!(:version3) do
           Version.create!(
             item_type: 'Visit',
             item_id: visit.id,
@@ -66,7 +104,7 @@ describe Migration::MigrateRequiredSeriesChanges, paper_trail: false do
                   'SPECT_1' => {
                     image_series_id: "5",
                     tqc_state: 2,
-                    tqc_results: {
+                     tqc_results: {
                       'question' => true
                     },
                     tqc_user_id: user.id,
@@ -85,12 +123,31 @@ describe Migration::MigrateRequiredSeriesChanges, paper_trail: false do
         end
 
         it 'updates the required series record' do
-
+          required_series.reload
+          expect(required_series.attributes)
+            .to include(
+                  'tqc_state' => 2,
+                  'tqc_results' => {
+                    'question' => true
+                  },
+                  'tqc_user_id' => user.id,
+                  'tqc_date' => date,
+                  'tqc_version' => nil,
+                  'tqc_comment' => ''
+                )
         end
         it 'creates versions for the respective required series update' do
           expect(Version.where(item_type: 'RequiredSeries').map(&:attributes))
             .to include(
                   include(
+                    'object' => include(
+                      'id' => required_series.id,
+                      'visit_id' => visit.id,
+                      'name' => 'SPECT_1',
+                      'created_at' => required_series.created_at.as_json,
+                      'image_series_id' => 5,
+                      'tqc_state' => 'pending'
+                    ),
                     'object_changes' => include(
                       'tqc_state' => ['pending', 'passed'],
                       'tqc_results' => [nil, {'question' => true}],
@@ -142,23 +199,24 @@ describe Migration::MigrateRequiredSeriesChanges, paper_trail: false do
 
       it 'extracts the changes of required series' do
         expect(required_series_changes).to include(
-                                             'visit_id' => 5, 'name' => 'SPECT_1',
-                                             'changes' => {
+                                             visit_id: 5,
+                                             name: 'SPECT_1',
+                                             changes: {
                                                'image_series_id' => [2, 5]
                                              }
                                            )
         expect(required_series_changes).to include(
-                                             'visit_id' => 5,
-                                             'name' => 'SPECT_2',
-                                             'changes' => {
+                                             visit_id: 5,
+                                             name: 'SPECT_2',
+                                             changes: {
                                                'image_series_id' => [nil, 7],
                                                'tqc_state' => [nil, 'pending']
                                              }
                                            )
         expect(required_series_changes).to include(
-                                             'visit_id' => 5,
-                                             'name' => 'SPECT_3',
-                                             'changes' => {
+                                             visit_id: 5,
+                                             name: 'SPECT_3',
+                                             changes: {
                                                'image_series_id' => [3, nil],
                                                'tqc_state' => ['pending', nil]
                                              }
