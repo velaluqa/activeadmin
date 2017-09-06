ced0ddbbfea06fb2fd0f8ace934e9aaa08797abd
Arthur Andersen <aandersen@velalu.qa>
2017-09-06 09:48:04 +0200

    Also fix version state columns for previous value

--- lib/migration/fix_version_object_changes_for_state_columns.rb
+++ lib/migration/fix_version_object_changes_for_state_columns.rb
@@ -2,39 +2,90 @@ module Migration
   class FixVersionObjectChangesForStateColumns
     class << self
       def run
+        progress = ProgressBar.create(
+          title: 'Visit#state',
+          total: visit_state_versions.count,
+          format: '%t |%B| %a / %E (%c / %C ~ %p%%)'
+        )
         visit_state_versions.find_each do |version|
-          version.object_changes['state'][1] =
-            Visit.state_sym_to_int(version.object_changes['state'][1].to_sym)
+          was, becomes = version.object_changes['state']
+          if was.is_a?(String)
+            version.object_changes['state'][0] =
+              Visit.state_sym_to_int(was.to_sym)
+          end
+          if becomes.is_a?(String)
+            version.object_changes['state'][1] =
+              Visit.state_sym_to_int(becomes.to_sym)
+          end
           version.save
+          progress.increment
         end
+        progress = ProgressBar.create(
+          title: 'Visit#mqc_state',
+          total: visit_mqc_state_versions.count,
+          format: '%t |%B| %a / %E (%c / %C ~ %p%%)'
+        )
         visit_mqc_state_versions.find_each do |version|
-          version.object_changes['mqc_state'][1] =
-            Visit.mqc_state_sym_to_int(version.object_changes['mqc_state'][1].to_sym)
+          was, becomes = version.object_changes['mqc_state']
+          if was.is_a?(String)
+            version.object_changes['mqc_state'][0] =
+              Visit.mqc_state_sym_to_int(was.to_sym)
+          end
+          if becomes.is_a?(String)
+            version.object_changes['mqc_state'][1] =
+              Visit.mqc_state_sym_to_int(becomes.to_sym)
+          end
           version.save
+          progress.increment
         end
+        progress = ProgressBar.create(
+          title: 'ImageSeries#state',
+          total: image_series_state_versions.count,
+          format: '%t |%B| %a / %E (%c / %C ~ %p%%)'
+        )
         image_series_state_versions.find_each do |version|
-          version.object_changes['state'][1] =
-            ImageSeries.state_sym_to_int(version.object_changes['state'][1].to_sym)
+          was, becomes = version.object_changes['state']
+          if was.is_a?(String)
+            version.object_changes['state'][0] =
+              ImageSeries.state_sym_to_int(was.to_sym)
+          end
+          if becomes.is_a?(String)
+            version.object_changes['state'][1] =
+              ImageSeries.state_sym_to_int(becomes.to_sym)
+          end
           version.save
+          progress.increment
         end
       end
 
       def visit_state_versions
         Version
           .where(item_type: 'Visit')
-          .where('"versions"."object_changes"::jsonb ? \'state\'')
+          .where(
+            'object_changes #>> \'{state,0}\' IN (?) OR object_changes #>> \'{state,1}\' IN (?)',
+            Visit::STATE_SYMS,
+            Visit::STATE_SYMS
+          )
       end
 
       def visit_mqc_state_versions
         Version
           .where(item_type: 'Visit')
-          .where('"versions"."object_changes"::jsonb ? \'mqc_state\'')
+          .where(
+            'object_changes #>> \'{mqc_state,0}\' IN (?) OR object_changes #>> \'{mqc_state,1}\' IN (?)',
+            Visit::MQC_STATE_SYMS,
+            Visit::MQC_STATE_SYMS
+          )
       end
 
       def image_series_state_versions
         Version
           .where(item_type: 'ImageSeries')
-          .where('"versions"."object_changes"::jsonb ? \'state\'')
+          .where(
+            'object_changes #>> \'{state,0}\' IN (?) OR object_changes #>> \'{state,1}\' IN (?)',
+            ImageSeries::STATE_SYMS,
+            ImageSeries::STATE_SYMS
+          )
       end
     end
   end
--- spec/lib/migration/fix_version_object_changes_for_state_columns_spec.rb
+++ spec/lib/migration/fix_version_object_changes_for_state_columns_spec.rb
@@ -14,6 +14,17 @@ describe Migration::FixVersionObjectChangesForStateColumns do
           state: [0, 'complete_tqc_passed']
         }
       )
+      @visit_state_version2 = Version.create(
+        item_type: 'Visit',
+        item_id: 1,
+        event: 'update',
+        object: {
+          state: 0
+        },
+        object_changes: {
+          state: ['incomplete_na', 'complete_tqc_passed']
+        }
+      )
       @visit_mqc_state_version = Version.create(
         item_type: 'Visit',
         item_id: 1,
@@ -23,6 +34,17 @@ describe Migration::FixVersionObjectChangesForStateColumns do
         },
         object_changes: {
           mqc_state: [0, 'issues']
+        }
+      )
+      @visit_mqc_state_version2 = Version.create(
+        item_type: 'Visit',
+        item_id: 1,
+        event: 'update',
+        object: {
+          mqc_state: 0
+        },
+        object_changes: {
+          mqc_state: ['pending', 'issues']
         }
       )
       @image_series_state_version = Version.create(
@@ -36,16 +58,33 @@ describe Migration::FixVersionObjectChangesForStateColumns do
           state: [0, 'imported']
         }
       )
+      @image_series_state_version2 = Version.create(
+        item_type: 'ImageSeries',
+        item_id: 1,
+        event: 'update',
+        object: {
+          state: 0
+        },
+        object_changes: {
+          state: ['importing', 1]
+        }
+      )
       Migration::FixVersionObjectChangesForStateColumns.run
       @visit_state_version.reload
+      @visit_state_version2.reload
       @visit_mqc_state_version.reload
+      @visit_mqc_state_version2.reload
       @image_series_state_version.reload
+      @image_series_state_version2.reload
     end
 
     it 'replaces symbol values in `object_changes` by their indexes' do
-      expect(@visit_state_version.object_changes.dig2('state', 1)).to eq(1)
-      expect(@visit_mqc_state_version.object_changes.dig2('mqc_state', 1)).to eq(1)
-      expect(@image_series_state_version.object_changes.dig2('state', 1)).to eq(1)
+      expect(@visit_state_version.object_changes['state']).to eq([0, 1])
+      expect(@visit_state_version2.object_changes['state']).to eq([0, 1])
+      expect(@visit_mqc_state_version.object_changes['mqc_state']).to eq([0, 1])
+      expect(@visit_mqc_state_version2.object_changes['mqc_state']).to eq([0, 1])
+      expect(@image_series_state_version.object_changes['state']).to eq([0, 1])
+      expect(@image_series_state_version2.object_changes['state']).to eq([0, 1])
     end
   end
 end
