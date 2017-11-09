ea4c318541900e4bad43f2fd1a4c74b87877ba16
Arthur Andersen <aandersen@velalu.qa>
2017-11-09 10:01:12 +0100

    Add `comment` ability to commentable resources

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,19 +6,19 @@ class Ability
   ACTIVITIES = {
     BackgroundJob => %i[manage read update create destroy],
     Sidekiq => %i[manage],
-    Study  => %i[manage read update create destroy read_reports],
-    Center => %i[manage read update create destroy],
-    Patient => %i[manage read update create destroy],
+    Study  => %i[manage read update create destroy comment read_reports],
+    Center => %i[manage read update create destroy comment],
+    Patient => %i[manage read update create destroy comment],
     EmailTemplate => %i[manage read update create destroy],
-    ImageSeries => %i[manage read update create destroy upload assign_patient assign_visit],
-    Image => %i[manage read update create destroy],
+    ImageSeries => %i[manage read update create destroy comment upload assign_patient assign_visit],
+    Image => %i[manage read update create destroy comment],
     NotificationProfile => %i[manage read update create destroy],
     Notification => %i[manage read update create destroy],
     User => %i[manage read update create destroy generate_keypair],
     UserRole => %i[manage read update create destroy],
     PublicKey => %i[manage read update create destroy],
     Role => %i[manage read update create destroy],
-    Visit => %i[manage read update create destroy create_from_template assign_required_series technical_qc medical_qc],
+    Visit => %i[manage read update create destroy comment create_from_template assign_required_series technical_qc medical_qc],
     Version => %i[manage read update create destroy]
   }.freeze
 
