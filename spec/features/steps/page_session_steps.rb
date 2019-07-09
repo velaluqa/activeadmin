98fa9bebd39bd78a24284a5fae8be3d02ebe9c4e
Arthur Andersen <aandersen@velalu.qa>
2019-07-09 16:16:17 +0200

    Add window management steps for feature tests

--- spec/features/steps/page_session_steps.rb
+++ spec/features/steps/page_session_steps.rb
@@ -256,3 +256,36 @@ step 'I download zip file' do
   end
   expect(no_downloads_el[:hidden]).to be_falsy
 end
+
+step 'another window is opened' do
+  other_windows =
+    (page.driver.browser.window_handles - [page.driver.browser.window_handle])
+  handle = other_windows.last
+  expect(handle).not_to be_nil
+  page.driver.browser.switch_to.window(handle)
+end
+
+step 'I switch to the window :string' do |window_name|
+  handle = page.driver.find_window(window_name)
+  page.driver.browser.switch_to.window(handle)
+end
+
+step 'I close the current window' do
+  page.driver.browser.switch_to.window(page.driver.browser.window_handle)
+  page.driver.browser.close
+end
+
+step 'I close the window :string' do |window_name|
+  handle = page.driver.browser.window_handle
+  send('I switch to the window :string', window_name)
+  send('I close the current window')
+  page.driver.browser.switch_to.window(handle)
+end
+
+step 'I confirm popup' do
+  page.driver.browser.switch_to.alert.accept
+end
+
+step 'I dismiss popup' do
+  page.driver.browser.switch_to.alert.dismiss
+end
