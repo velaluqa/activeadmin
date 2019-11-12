8cdfc8f11cddca4cc36c721cdd19ebc78a1d263a
Arthur Andersen <aandersen@velalu.qa>
2019-11-12 13:52:56 +0100

    Use `Study::UploadConfiguration` for `:study` factory

--- spec/factories/study.rb
+++ spec/factories/study.rb
@@ -26,7 +26,22 @@ FactoryBot.define do
 
     after(:create) do |study, evaluator|
       if evaluator.configuration.is_a?(String)
-        study.update_configuration!(evaluator.configuration)
+        temp_file = Tempfile.new.tap do |file|
+          file.write(evaluator.configuration)
+          file.close
+        end
+
+        result = Study::UploadConfiguration.(
+          params: {
+            id: study.id,
+            'study_contract_upload_configuration' => {
+              id: study.id,
+              file: Rack::Test::UploadedFile.new(temp_file.path)
+            }
+          }
+        )
+
+        raise result['contract.default'].errors.messages.inspect unless result.success?
       end
     end
 
