e787cb07f70bf955fb7b37438604aa50991ff752
Tochukwu <t.uzochukwu@velalu.qa>
2022-04-13 01:30:30 +0100

    implemented forgot password feature and wrote test implementations for the feature

--- spec/features/steps/mail_steps.rb
+++ spec/features/steps/mail_steps.rb
@@ -7,3 +7,21 @@ step 'I receive an e-mail to :string with subject :string' do |email, subject|
   step "I open the mail inbox"
   step "I see \"#{subject}\" in \"#{email}\" row"
 end
+
+step 'I open the mail to :string with subject :string' do |recipient, subject|
+  within("tr", text: recipient) do
+    page.find("td", text: subject).click
+  end
+end
+
+step 'I click the link :string within the mail' do |link_text|
+  within_frame(page.find("iframe")) do
+    page.find_link(link_text).click
+  end
+end
+
+step 'I click :string in the :string e-mail sent to :string' do |link_text, subject, recipient|
+  step 'I open the mail inbox'
+  step "I open the mail to \"#{recipient}\" with subject \"#{subject}\""
+  step "I click the link \"#{link_text}\" within the mail"
+end
