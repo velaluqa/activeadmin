7042824f2cfe30cb9dc3fb401f0d97d2df78030f
Tochukwu <t.uzochukwu@velalu.qa>
2022-05-25 10:55:09 +0100

    Check validity before granting permission in feature test step

--- spec/features/steps/role_steps.rb
+++ spec/features/steps/role_steps.rb
@@ -3,6 +3,9 @@ step 'a role :string with permissions:' do |title, permissions|
   permissions.to_a.each do |subject, activities|
     activities = activities.split(/, ?/)
     activities.each do |activity|
+      unless Ability::ACTIVITIES[subject.constantize].include?(activity.to_sym)
+        fail "Permission #{activity} not known for subject #{subject}"
+      end
       role.add_permission(activity, subject)
     end
   end
