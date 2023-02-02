2975be9b10aa36b2544eef33f3fd38dfbda088fb
Arthur Andersen <aandersen@velalu.qa>
2023-02-02 10:25:00 +0100

    (#6102) Fix `Simulate Recipients` for activeadmin comments

--- lib/record_search.rb
+++ lib/record_search.rb
@@ -65,7 +65,7 @@ class RecordSearch
     when "User" then User.find(id)
     when "Role" then Role.find(id)
     when "RequiredSeries" then RequiredSeries.find(id)
-    when "ActiveAdmin::Comments" then ActiveAdmin::Comments.find(id)
+    when "ActiveAdmin::Comment", "Comment" then ActiveAdmin::Comment.find(id)
     when "FormAnswer" then FormAnswer.find(id)
     else fail "Unknown record search type #{type.inspect}"
     end
